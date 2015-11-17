ee42a78f61f514c2abddd264adc301a94c18fbc4
Holger Caesar <holger@calvinn.inf.ed.ac.uk>
2015-11-17 14:56:53 +0000

    Fixed three minor bugs

--- matlab/@CalvinNN/convertNetwork.m
+++ matlab/@CalvinNN/convertNetwork.m
@@ -27,7 +27,7 @@ net.replaceLayer('prob', 'softmaxloss', softmaxlossLayer, 'label');
 
 % Adapt number of classes in softmaxloss layer from 1000 to labelCount
 fc8Idx = net.getLayerIndex('fc8');
-net.layers(fc8Idx).block.size(4) = obj.imdb.labelCount;
+net.layers(fc8Idx).block.size(4) = obj.imdb.numClasses;
 newParams = net.layers(fc8Idx).block.initParams();
 net.params(net.layers(fc8Idx).paramIndexes(1)).value = newParams{1};
 net.params(net.layers(fc8Idx).paramIndexes(2)).value = newParams{2};
