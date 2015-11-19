87730997b77638634a3d5b217647eedcc6a1e1c5
Holger Caesar <holger.caesar@ed.ac.uk>
2015-11-19 11:09:40 +0000

    Changed saveState to dynamically decide matfile version

--- matlab/@CalvinNN/saveState.m
+++ matlab/@CalvinNN/saveState.m
@@ -2,7 +2,19 @@ function saveState(fileName, net, stats) %#ok<INUSD>
 % saveState(fileName, net, stats)
 %
 % Save network and statistics for the current epoch.
-% Files are saved in v7.3 format to allow nets with > 4GB memory.
+% Files are saved in v7.3 format to allow nets with > 2GB memory.
 
+% Extract the fields from the DAG class
 net = net.saveobj() ; %#ok<NASGU>
-save(fileName, 'net', 'stats', '-v7.3') ;
+
+% Determine whether to use new matfile format
+netInfo = whos('net');
+netSize = netInfo.bytes;
+if netSize > 2e9,
+    matVer = '-v7.3';
+else
+    matVer = '-v6';
+end;
+
+% Save to file
+save(fileName, 'net', 'stats', matVer);
