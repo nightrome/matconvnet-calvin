7cee4e6749f66d342edf672f1e352d11a5f4e17b
Holger Caesar <holger.caesar@ed.ac.uk>
2015-11-20 15:02:14 +0000

    simplified accumulateStats

--- matlab/@CalvinNN/accumulateStats.m
+++ matlab/@CalvinNN/accumulateStats.m
@@ -1,29 +1,26 @@
-function stats = accumulateStats(stats_)
-% stats = accumulateStats(stats_)
+function stats = accumulateStats(obj, stats_)
+% stats = accumulateStats(obj, stats_)
 
-stats = struct() ;
+stats = struct();
+datasetMode = obj.imdb.datasetMode;
+total = 0;
 
-for s = {'train', 'val'}
-  s = char(s) ;
-  total = 0 ;
-
-  for g = 1:numel(stats_)
-    stats__ = stats_{g} ;
-    num__ = stats__.(s).num ;
-    total = total + num__ ;
-
-    for f = setdiff(fieldnames(stats__.(s))', 'num')
-      f = char(f) ;
-
-      if g == 1
-        stats.(s).(f) = 0 ;
-      end
-      stats.(s).(f) = stats.(s).(f) + stats__.(s).(f) * num__ ;
-
-      if g == numel(stats_)
-        stats.(s).(f) = stats.(s).(f) / total ;
-      end
+for g = 1:numel(stats_)
+    stats__ = stats_{g};
+    num__ = stats__.(datasetMode).num;
+    total = total + num__;
+    
+    for f = setdiff(fieldnames(stats__.(datasetMode))', 'num')
+        f = char(f);
+        
+        if g == 1
+            stats.(datasetMode).(f) = 0;
+        end
+        stats.(datasetMode).(f) = stats.(datasetMode).(f) + stats__.(datasetMode).(f) * num__;
+        
+        if g == numel(stats_)
+            stats.(datasetMode).(f) = stats.(datasetMode).(f) / total;
+        end
     end
-  end
-  stats.(s).num = total ;
 end
+stats.(datasetMode).num = total;
