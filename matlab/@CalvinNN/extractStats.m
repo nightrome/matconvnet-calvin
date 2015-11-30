5851e60c65728d62f3aca8d6867b53492e0b8110
Holger Caesar <holger.caesar@ed.ac.uk>
2015-11-30 17:01:37 +0000

    Moved optional nnOpts to nnOpts.misc Set default learning rate to change after 12 epochs Changed to default matconvnet style of parsing arguments

--- matlab/@CalvinNN/extractStats.m
+++ matlab/@CalvinNN/extractStats.m
@@ -3,8 +3,8 @@ function stats = extractStats(net, ~)
 %
 % Extract all losses from the network.
 
-sel = find(cellfun(@(x) isa(x,'dagnn.Loss'), {net.layers.block})) ;
-stats = struct() ;
+sel = find(cellfun(@(x) isa(x, 'dagnn.Loss'), {net.layers.block}));
+stats = struct();
 for i = 1:numel(sel)
-  stats.(net.layers(sel(i)).outputs{1}) = net.layers(sel(i)).block.average ;
+  stats.(net.layers(sel(i)).outputs{1}) = net.layers(sel(i)).block.average;
 end
