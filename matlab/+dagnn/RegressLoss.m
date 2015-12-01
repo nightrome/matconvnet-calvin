f16c54358487fca855dc5d5520dd2ba89c832607
Jasper <jrr.uijlings@ed.ac.uk>
2015-12-01 15:37:39 +0000

    Regression Loss now has NaNs as non-target values. BBox regression updated to one regression target per class.

--- matlab/+dagnn/RegressLoss.m
+++ matlab/+dagnn/RegressLoss.m
@@ -1,31 +1,37 @@
 classdef RegressLoss < dagnn.Loss
-
-  methods
-    function outputs = forward(obj, inputs, params)
-      lossWeight = repmat(double(inputs{3} ~= 1), [1 4]);
-      inputs{2} = inputs{2} .* lossWeight;
-      inputs{1} = inputs{1} .* permute(lossWeight, [4 3 2 1]);
-        
-      outputs{1} = vl_nnloss_regress(inputs{1}, inputs{2}, [], 'loss', obj.loss) ;
-      n = obj.numAveraged ;
-      m = n + size(inputs{1},4) ;
-      obj.average = (n * obj.average + gather(outputs{1})) / m ;
-      obj.numAveraged = m ;
-    end
-
-    function [derInputs, derParams] = backward(obj, inputs, params, derOutputs)
-      lossWeight = repmat(double(inputs{3} ~= 1), [1 4]);
-      inputs{2} = inputs{2} .* lossWeight;
-      inputs{1} = inputs{1} .* permute(lossWeight, [4 3 2 1]);
-
-      derInputs{1} = vl_nnloss_regress(inputs{1}, inputs{2}, derOutputs{1}, 'loss', obj.loss) ;
-      derInputs{2} = [] ;
-      derInputs{3} = [] ;
-      derParams = {} ;
-    end
     
-    function obj = RegressLoss(varargin)
-      obj.load(varargin) ;
+    methods
+        function outputs = forward(obj, inputs, params)
+            % Deal with NaNs in target scores which should be ignored
+            isnanMask = isnan(inputs{2});
+            regressionTargets = inputs{2};
+            regressionTargets(isnanMask) = 0;
+            regressionScore = inputs{1};
+            regressionScore(isnanMask) = 0;
+            
+            outputs{1} = vl_nnloss_regress(regressionScore, regressionTargets, [], 'loss', obj.loss) ;
+            n = obj.numAveraged ;
+            m = n + size(inputs{1},4) ;
+            obj.average = (n * obj.average + gather(outputs{1})) / m ;
+            obj.numAveraged = m ;
+        end
+        
+        function [derInputs, derParams] = backward(obj, inputs, params, derOutputs)
+            % Deal with NaNs in target scores which should be ignored
+            isnanMask = isnan(inputs{2});
+            regressionTargets = inputs{2};
+            regressionTargets(isnanMask) = 0;
+            regressionScore = inputs{1};
+            regressionScore(isnanMask) = 0;
+            
+            derInputs{1} = vl_nnloss_regress(regressionScore,regressionTargets, derOutputs{1}, 'loss', obj.loss) ;
+            derInputs{2} = [] ;
+            derInputs{3} = [] ;
+            derParams = {} ;
+        end
+        
+        function obj = RegressLoss(varargin)
+            obj.load(varargin) ;
+        end
     end
-  end
 end
