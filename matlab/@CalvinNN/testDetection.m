6f85d15bd7185fd76956c95f053d0db053772276
Jasper <jrr.uijlings@ed.ac.uk>
2015-12-01 14:26:11 +0000

    Added regression option

--- matlab/@CalvinNN/testDetection.m
+++ matlab/@CalvinNN/testDetection.m
@@ -35,6 +35,15 @@ inputNames = inputs(1:2:end);
 boxI = boxI * 2; % Index of actual argument
 boxes = inputs{boxI};
 
+% Get regression targets for boxes
+if imdb.boxRegress
+    vI = net.getVarIndex('xRegress');
+    regressStruct = net.vars(vI);
+    regressFactors = permute(regressStruct.value, [4 3 2 1]);
+else
+    regressFactors = zeros(size(boxes));
+end
+
 % Get top boxes for each category. Perform NMS. Thresholds defined at top of function
 currMaxBoxes = min(maxNumBoxesPerImTest, size(boxes, 1));
 for cI = size(scores,2):-1:1
@@ -44,16 +53,26 @@ for cI = size(scores,2):-1:1
     sI = sI(1:currMaxBoxes);
     currBoxes = boxes(sI,:);
     
+    % Do regression
+    currRegressF = regressFactors(sI,:);
+    currBoxesReg = BoxRegresss(currBoxes, currRegressF);
+    
     % Get scores (w boxes) above certain threshold
     goodI = currScores > minDetectionScore;
     currScores = currScores(goodI,:);
     currBoxes = currBoxes(goodI,:);
+    currBoxesReg = currBoxesReg(goodI,:);
     
     % Perform NMS
     [~, goodBoxesI] = BoxNMS(currBoxes, nmsTTest);
     currBoxes = currBoxes(goodBoxesI,:);
     currScores = currScores(goodBoxesI,:);
+    currBoxesReg = currBoxesReg(goodBoxesI,:);
     
     results.boxes{cI} = gather(currBoxes);
     results.scores{cI} = gather(currScores);
+    
+    if imdb.boxRegress
+        results.regress{cI} = currBoxesReg;
+    end
 end
