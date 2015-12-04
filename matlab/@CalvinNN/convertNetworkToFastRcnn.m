a5caa3c8c848b99361e8a0347a145c6eb5e4e62a
Holger Caesar <holger.caesar@ed.ac.uk>
2015-12-04 11:40:18 +0000

    Enabled (optional) duplication of weights for freeform pooling, instead of weight sharing between foreground and box activations

--- matlab/@CalvinNN/convertNetworkToFastRcnn.m
+++ matlab/@CalvinNN/convertNetworkToFastRcnn.m
@@ -1,7 +1,9 @@
 function convertNetworkToFastRcnn(obj)
 % convertNetworkToFastRcnn(obj)
-
+%
 % Modify network for Fast R-CNN's ROI pooling
+%
+% Copyright by Holger Caesar, 2015
 
 % Replace max-pooling layer by ROI pooling
 fc6Idx = obj.net.getLayerIndex('fc6');
@@ -13,7 +15,38 @@ obj.net.replaceLayer('pool5', 'roipool5', roiPoolBlock, {'oriImSize', 'boxes'}, 
 if isfield(obj.nnOpts.misc, 'roiPool'),
     roiPool = obj.nnOpts.misc.roiPool;
     if isfield(roiPool, 'freeform') && roiPool.freeform.use
+        % Compute activations for foreground and entire box separately
+        % (by default off)
         roiPoolFreeformBlock = dagnn.RoiPoolingFreeform('combineFgBox', roiPool.freeform.combineFgBox);
         obj.net.insertLayer('roipool5', 'fc6', 'roipoolfreeform5', roiPoolFreeformBlock, 'blobMasks');
+        
+        % Share fully connected layer weights for foreground and entire box
+        % (by default on)
+        if isfield(roiPool.freeform, 'shareWeights') && ~roiPool.freeform.shareWeights
+            relLayers = {'fc6', 'fc7', 'fc8'};
+            for relIdx = 1 : numel(relLayers),
+                relLayer = relLayers{relIdx};
+                relLayerIdx = obj.net.getLayerIndex(relLayer);
+                paramIndexes = obj.net.layers(relLayerIdx).paramIndexes;
+                
+                % Duplicate input size for all but the first fc layer
+                if relIdx > 1
+                    obj.net.layers(relLayerIdx).block.size(3) = obj.net.layers(relLayerIdx).block.size(3) * 2;
+                    obj.net.params(paramIndexes(1)).value = cat(3, ...
+                        obj.net.params(paramIndexes(1)).value, ...
+                        obj.net.params(paramIndexes(1)).value);
+                end
+                % Duplicate output size for all but the last fc layers
+                if relIdx < numel(relLayers)
+                    obj.net.layers(relLayerIdx).block.size(4) = obj.net.layers(relLayerIdx).block.size(4) * 2;
+                    obj.net.params(paramIndexes(1)).value = cat(4, ...
+                        obj.net.params(paramIndexes(1)).value, ...
+                        obj.net.params(paramIndexes(1)).value);
+                    obj.net.params(paramIndexes(2)).value = cat(2, ...
+                        obj.net.params(paramIndexes(2)).value, ...
+                        obj.net.params(paramIndexes(2)).value);
+                end
+            end
+        end
     end
 end;
