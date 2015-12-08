3e2d602403f1ad9cd262700b4968f357da01ef62
Holger Caesar <holger.caesar@ed.ac.uk>
2015-12-08 16:54:54 +0000

    Added new parameter to replaceLayer

--- matlab/+dagnn/@DagNN/replaceLayer.m
+++ matlab/+dagnn/@DagNN/replaceLayer.m
@@ -1,5 +1,5 @@
-function replaceLayer(obj, layerName, newLayerName, newBlock, addInputs, addOutputs, addParams)
-% replaceLayer(obj, layerName, newLayerName, newBlock, [addInputs], [addOutputs], [addParams])
+function replaceLayer(obj, layerName, newLayerName, newBlock, addInputs, addOutputs, addParams, removeOldInputs)
+% replaceLayer(obj, layerName, newLayerName, newBlock, [addInputs], [addOutputs], [addParams], [removeOldInputs])
 %
 % Takes a DAG and replaces an existing layer with a new one.
 % If not specified, the inputs, outputs and params are reused from the
@@ -7,23 +7,34 @@ function replaceLayer(obj, layerName, newLayerName, newBlock, addInputs, addOutp
 %
 % Copyright by Holger Caesar, 2015
 
+% Default settings
+if ~exist('removeOldInputs', 'var') || isempty(removeOldInputs),
+    removeOldInputs = false;
+end;
+
 % Find the old layer
 layerIdx = obj.getLayerIndex(layerName);
 layer = obj.layers(layerIdx);
 
-newInputs  = layer.inputs;
+% Keep old settings (or not)
+if removeOldInputs
+    newInputs = {};
+else
+    newInputs = layer.inputs;
+end
 newOutputs = layer.outputs;
 newParams  = layer.params;
 
-if exist('addInputs', 'var') && ~isempty(addInputs),
+% Add new settings
+if exist('addInputs', 'var') && ~isempty(addInputs)
     newInputs = [newInputs, addInputs];
-end;
-if exist('addOutputs', 'var') && ~isempty(addOutputs),
+end
+if exist('addOutputs', 'var') && ~isempty(addOutputs)
     newOutputs = [newOutputs, addOutputs];
-end;
-if exist('addParams', 'var') && ~isempty(addParams),
+end
+if exist('addParams', 'var') && ~isempty(addParams)
     newParams = [newParams, addParams];
-end;
+end
 
 % Remove the old layer (must come before add to avoid conflicts)
 obj.removeLayer(layerName);
