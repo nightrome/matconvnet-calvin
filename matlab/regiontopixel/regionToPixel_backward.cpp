359b0ec519dac6cec18f8580023bc15f9aa82404
Holger Caesar <holger.caesar@ed.ac.uk>
2015-12-08 10:10:08 +0000

    Renamed variables

--- matlab/regiontopixel/regionToPixel_backward.cpp
+++ matlab/regiontopixel/regionToPixel_backward.cpp
@@ -2,7 +2,7 @@
 #include "mex.h"
 
 /*
- * [dzdxout] = regiontopixel_backward(boxCount, spMap, dzdx)
+ * [dzdx] = regionToPixel_backward(boxCount, spMap, dzdy)
  *
  * Go from a pixel level back to region level.
  * This uses the mask saved in the forward pass.
@@ -15,14 +15,14 @@ void mexFunction(int nlhs, mxArray *out[], int nrhs, const mxArray *input[])
     if (nlhs == 0) {
         return;
     } else if (nlhs != 1 || nrhs != 3) {
-        mexErrMsgTxt("Error. Usage: [dzdxout] = regiontopixel_backward(boxCount, spMap, dzdx)");
+        mexErrMsgTxt("Error. Usage: [dzdx] = regionToPixel_backward(boxCount, spMap, dzdy)");
         return;
     }
     
     // Get pointers
     const mxArray* boxCountMx = input[0];
     const mxArray* spMapMx = input[1];
-    const mxArray* dzdxMx = input[2];
+    const mxArray* dzdyMx = input[2];
     
     // Check inputs
     if (!mxIsDouble(boxCountMx) || !mxIsScalar(boxCountMx)) {
@@ -33,26 +33,26 @@ void mexFunction(int nlhs, mxArray *out[], int nrhs, const mxArray *input[])
     }
     int labelCount = mxGetM(spMapMx);
     int spCount    = mxGetN(spMapMx);
-    const mwSize* dzdxDims = mxGetDimensions(dzdxMx);
-    if (!mxIsSingle(dzdxMx) || dzdxDims[0] != 1 || dzdxDims[1] != 1 || dzdxDims[2] != labelCount ||
-              (!(mxGetNumberOfDimensions(dzdxMx) == 4 && dzdxDims[3] == spCount)
-            && !(mxGetNumberOfDimensions(dzdxMx) == 3))) {
-        mexErrMsgTxt("Error: dzdx must be single with format 1 x 1 x labelCount x spCount!");
+    const mwSize* dzdyDims = mxGetDimensions(dzdyMx);
+    if (!mxIsSingle(dzdyMx) || dzdyDims[0] != 1 || dzdyDims[1] != 1 || dzdyDims[2] != labelCount ||
+              (!(mxGetNumberOfDimensions(dzdyMx) == 4 && dzdyDims[3] == spCount)
+            && !(mxGetNumberOfDimensions(dzdyMx) == 3))) {
+        mexErrMsgTxt("Error: dzdy must be single with format 1 x 1 x labelCount x spCount!");
     }
     
     // Get arrays
     int boxCount  = (int) mxGetScalar(boxCountMx);
     double* spMap = (double*) mxGetData(spMapMx);
-    float* dzdx   = (float*) mxGetData(dzdxMx);
+    float* dzdy   = (float*) mxGetData(dzdyMx);
     
     // Create output and initialize it to all zeros (in mxCreateNumericArray)
-    mwSize dzdxoutSize[4];
-    dzdxoutSize[0] = 1;
-    dzdxoutSize[1] = 1;
-    dzdxoutSize[2] = labelCount;
-    dzdxoutSize[3] = boxCount;
-    out[0] = mxCreateNumericArray(4, dzdxoutSize, mxSINGLE_CLASS, mxREAL);
-    float* dzdxout = (float*) mxGetData(out[0]);
+    mwSize dzdxSize[4];
+    dzdxSize[0] = 1;
+    dzdxSize[1] = 1;
+    dzdxSize[2] = labelCount;
+    dzdxSize[3] = boxCount;
+    out[0] = mxCreateNumericArray(4, dzdxSize, mxSINGLE_CLASS, mxREAL);
+    float* dzdx = (float*) mxGetData(out[0]);
     
     for (int spIdx = 0; spIdx < spCount; spIdx++) {
         for (int labelIdx = 0; labelIdx < labelCount; labelIdx++) {
@@ -62,8 +62,8 @@ void mexFunction(int nlhs, mxArray *out[], int nrhs, const mxArray *input[])
             double boxIdxD = spMap[spMapIdx];
             int boxIdx = (int) boxIdxD - 1; // Convert from Matlab to C indexing
             if (!mxIsNaN(boxIdxD)) {
-                int dzdxoutIdx = labelIdx + boxIdx * labelCount;
-                dzdxout[dzdxoutIdx] = dzdxout[dzdxoutIdx] + dzdx[spMapIdx];
+                int dzdxIdx = labelIdx + boxIdx * labelCount;
+                dzdx[dzdxIdx] = dzdx[dzdxIdx] + dzdy[spMapIdx];
             }
         }
     }
