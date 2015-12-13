d8f03d316cf9b5c885ac4d42a27a45b2fc9867e5
Jasper <jrr.uijlings@ed.ac.uk>
2015-12-13 13:08:11 +0000

    Modified regression loss. Testing only now..

--- matlab/+dagnn/RegressLoss.m
+++ matlab/+dagnn/RegressLoss.m
@@ -4,30 +4,57 @@ classdef RegressLoss < dagnn.Loss
     end
     
     methods
-        function outputs = forward(obj, inputs, params)
+        function outputs = forward(obj, inputs, params) %#ok<INUSD>
             % Deal with NaNs in target scores which should be ignored
             regressionTargets = inputs{2};
             isnanMask = isnan(regressionTargets);
             regressionTargets(isnanMask) = 0;
-            regressionScore = permute(inputs{1}, [4 3 2 1]);
+            regressionScore = squeeze(inputs{1});
+            assert(isequal(size(regressionTargets), size(regressionScore)));
             regressionScore(isnanMask) = 0;
             
-            outputs{1} = vl_nnloss_regress(regressionScore, regressionTargets, [], 'loss', obj.loss, 'smoothMaxDiff', obj.smoothMaxDiff) ;
+            % Get instanceWeights if specified
+            inputNames = obj.net.layers(obj.layerIndex).inputs;
+            [tf, iwInd] = ismember('instanceWeights', inputNames);
+            if tf
+                instanceWeights = inputs{iwInd};
+            else
+                instanceWeights = [];
+            end
+            
+            % Get loss
+            outputs{1} = vl_nnloss_regress(regressionScore, regressionTargets, [], ... 
+                'loss', obj.loss, 'smoothMaxDiff', obj.smoothMaxDiff, 'instanceWeights', instanceWeights);
+            
             n = obj.numAveraged ;
             m = n + size(inputs{1},4) ;
             obj.average = (n * obj.average + gather(outputs{1})) / m ;
             obj.numAveraged = m ;
         end
         
-        function [derInputs, derParams] = backward(obj, inputs, params, derOutputs)
+        function [derInputs, derParams] = backward(obj, inputs, params, derOutputs) %#ok<INUSL>
             % Deal with NaNs in target scores which should be ignored
             regressionTargets = inputs{2};
             isnanMask = isnan(regressionTargets);
             regressionTargets(isnanMask) = 0;
-            regressionScore = permute(inputs{1}, [4 3 2 1]);
+            regressionScore = squeeze(inputs{1});
+            assert(isequal(size(regressionTargets), size(regressionScore)));
             regressionScore(isnanMask) = 0;
             
-            derInputs{1} = vl_nnloss_regress(regressionScore,regressionTargets, derOutputs{1}, 'loss', obj.loss, 'smoothMaxDiff', obj.smoothMaxDiff) ;
+            % Get instanceWeights if specified
+            inputNames = obj.net.layers(obj.layerIndex).inputs;
+            [tf, iwInd] = ismember('instanceWeights', inputNames);
+            if tf
+                instanceWeights = inputs{iwInd};
+            else
+                instanceWeights = [];
+            end
+            
+            % Get gradient
+            derInputs{1} = vl_nnloss_regress(regressionScore,regressionTargets, derOutputs{1}, ...
+                'loss', obj.loss, 'smoothMaxDiff', obj.smoothMaxDiff, 'instanceWeights', instanceWeights);
+
+            derInputs{1} = reshape(derInputs{1}, size(inputs{1}));
             derInputs{2} = [] ;
             derInputs{3} = [] ;
             derParams = {} ;
