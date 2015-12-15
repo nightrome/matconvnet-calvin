a58567eb893afcea8d37e09df193b56ea030df52
Jasper <jrr.uijlings@ed.ac.uk>
2015-12-15 11:52:38 +0000

    Added detection example for Pascal

---
+++ examples/pascalDetection/DetectionToPascalVOCFiles.m
@@ -0,0 +1,66 @@
+function [recall, prec, ap, apUpperBound] = ...
+    DetectionToPascalVOCFiles(set, class, boxes, boxIms, boxClfs, compName, doEval, filterF)
+% Filters overlapping boxes (near duplicates), creates official VOC
+% detection files. Evaluates results.
+
+global DATAopts;
+if strcmp(set, 'test')
+    DATAopts = VOCinit(DATAopts.year, 1);
+end
+
+DATAopts.testset = set;
+
+if ~exist('doEval', 'var')
+    doEval = 0;
+end
+
+savePath = [DATAopts.resdir 'Main/%s_det_' set '_%s.txt']
+
+className = DATAopts.classes{class};
+
+% Sort scores/boxes/images
+[boxClfs sI] = sort(boxClfs, 'descend');
+boxIms = boxIms(sI);
+boxes = boxes(sI,:);
+
+% Filter boxes if wanted
+if exist('filterF', 'var')
+    if filterF > 0
+        [uIms uM uN] = unique(boxIms);
+        keepIds = true(size(boxes,1), 1);
+        fprintf('Filtering %d: ', length(uIms));
+        for i=1:length(uIms)
+            if mod(i,500) == 0
+                fprintf('%d ', i);
+            end
+            currIds = find(uN == i);
+            [filteredBoxes, goodBoxesI] = BoxNMS(boxes(currIds,:), filterF);
+            keepIds(currIds) = goodBoxesI;
+        end
+        boxClfs = boxClfs(keepIds);
+        boxIms = boxIms(keepIds);
+        boxes = boxes(keepIds,:);
+        fprintf('\n');
+    end
+end
+
+
+
+% Save detection results using detection results
+resultsName = sprintf(savePath, compName, className);
+fid = fopen(resultsName,'w');
+for j=1:length(boxIms)
+    fprintf(fid,'%s %f %f %f %f %f\n', boxIms{j}, boxClfs(j),boxes(j,:)));
+end
+fclose(fid);
+fprintf('\n');
+
+if doEval
+    [recall, prec, ap] = VOCevaldet2(DATAopts, className, resultsName, false);
+    apUpperBound = max(recall);
+else
+    recall = 0;
+    prec = 0;
+    ap = 0;
+    apUpperBound = 0;
+end
