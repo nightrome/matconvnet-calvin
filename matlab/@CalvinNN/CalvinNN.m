a5765592804f53682046eb21c6ba4ae4c94c33f6
Jasper <jrr.uijlings@ed.ac.uk>
2015-12-17 16:05:16 +0000

    minor bugfix

--- matlab/@CalvinNN/CalvinNN.m
+++ matlab/@CalvinNN/CalvinNN.m
@@ -65,7 +65,7 @@ classdef CalvinNN < handle
             elseif isfield(netIn, 'layers'),
                 % Convert a simpleNN network to DAG format
                 obj.convertNetwork(netIn);
-                obj.convertNetworkToFastRcnn2();
+                obj.convertNetworkToFastRcnn();
             else
                 error('Error: Network is neither in SimpleNN nor DAG format!');
             end
