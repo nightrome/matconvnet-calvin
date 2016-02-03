886b926d381c0526ef23e17fb5b7d00697befdb5
Holger Caesar <holger.caesar@ed.ac.uk>
2016-02-03 17:14:22 +0000

    Allowed for changed variable names in beta18 default networks (prediction->x%d+, x0->input only if necessary)

--- matlab/@CalvinNN/convertNetwork.m
+++ matlab/@CalvinNN/convertNetwork.m
@@ -12,16 +12,26 @@ dropout7Layer = dagnn.DropOut();
 obj.net.insertLayer('relu6', 'fc7', 'dropout6', dropout6Layer);
 obj.net.insertLayer('relu7', 'fc8', 'dropout7', dropout7Layer);
 
+% Rename variable prob to x%d+ style to make insertLayer work
+% Required for beta18 matconvnet default networks
+predVarIdx = obj.net.getVarIndex('prediction');
+if ~isnan(predVarIdx)
+    freeVarName = obj.net.getFreeVariable();
+    obj.net.renameVar('prediction', freeVarName);
+end
+
 % Replace softmax with correct loss for training (default: softmax)
 switch obj.nnOpts.lossFnObjective
     case 'softmaxlog'
         softmaxlossBlock = dagnn.LossWeighted('loss', 'softmaxlog');
         obj.net.replaceLayer('prob', 'softmaxloss', softmaxlossBlock, 'label');
-        obj.net.renameVar(obj.net.layers(obj.net.getLayerIndex('softmaxloss')).outputs, 'objective');
+        objectiveName = obj.net.layers(obj.net.getLayerIndex('softmaxloss')).outputs;
+        obj.net.renameVar(objectiveName, 'objective');
     case 'hinge'
         hingeLossBlock = dagnn.Loss('loss', 'hinge');
         obj.net.replaceLayer('prob', 'hingeloss', hingeLossBlock, 'label');
-        obj.net.renameVar(obj.net.layers(obj.net.getLayerIndex('hingeloss')).outputs, 'objective');
+        objectiveName = obj.net.layers(obj.net.getLayerIndex('hingeloss')).outputs;
+        obj.net.renameVar(objectiveName, 'objective');
     otherwise
         error('Wrong loss specified');
 end
@@ -34,7 +44,13 @@ obj.net.params(obj.net.layers(fc8Idx).paramIndexes(1)).value = newParams{1} / st
 obj.net.params(obj.net.layers(fc8Idx).paramIndexes(2)).value = newParams{2}';
 
 % Rename input
-obj.net.renameVar('x0', 'input');
+if ~isnan(obj.net.getVarIndex('x0'))
+    % Input variable x0 is renamed to input
+    obj.net.renameVar('x0', 'input');
+else
+    % Input variable already has the correct name
+    assert(~isnan(obj.net.getVarIndex('input')));
+end
 
 % Modify for Fast Rcnn (ROI pooling, bbox regression etc.)
 if obj.nnOpts.fastRcnn
