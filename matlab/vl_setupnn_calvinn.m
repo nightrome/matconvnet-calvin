57438271d487f5727ce7074b5f1073c2df6a3c7c
Holger Caesar <holger.caesar@ed.ac.uk>
2016-03-24 18:30:30 +0000

    Added missing addpath statements

--- matlab/vl_setupnn_calvinn.m
+++ matlab/vl_setupnn_calvinn.m
@@ -7,9 +7,10 @@ vl_setupnn();
 root = vl_rootnn();
 
 % New includes necessary for matconvnet-calvin
+addpath(fullfile(root, 'matlab', 'boxes'));
 addpath(fullfile(root, 'matlab', 'imdb'));
+addpath(fullfile(root, 'matlab', 'misc'));
+addpath(fullfile(root, 'matlab', 'labelpresence'));
 addpath(fullfile(root, 'matlab', 'regiontopixel'));
 addpath(fullfile(root, 'matlab', 'roipool'));
-addpath(fullfile(root, 'matlab', 'boxes'));
-addpath(fullfile(root, 'matlab', 'misc'));
 addpath(fullfile(root, 'examples', 'e2s2'));
