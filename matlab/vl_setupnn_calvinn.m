33934ecf6c53673cb3c97519db7641f61199f845
Holger Caesar <holger.caesar@ed.ac.uk>
2016-04-01 12:26:04 +0100

    Changed the setup file (removed deleted folders, added VOC example)

--- matlab/vl_setupnn_calvinn.m
+++ matlab/vl_setupnn_calvinn.m
@@ -7,10 +7,9 @@ vl_setupnn();
 root = vl_rootnn();
 
 % New includes necessary for matconvnet-calvin
-addpath(fullfile(root, 'matlab', 'boxes'));
 addpath(fullfile(root, 'matlab', 'imdb'));
-addpath(fullfile(root, 'matlab', 'misc'));
 addpath(fullfile(root, 'matlab', 'labelpresence'));
 addpath(fullfile(root, 'matlab', 'regiontopixel'));
 addpath(fullfile(root, 'matlab', 'roipool'));
 addpath(fullfile(root, 'examples', 'e2s2'));
+addpath(genpath(fullfile(root, 'examples', 'pascalDetection')));
