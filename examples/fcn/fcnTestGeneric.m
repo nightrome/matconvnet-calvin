a15a578109476157f0263efb6c8de7893dd90ff9
Holger Caesar <holger.caesar@ed.ac.uk>
2016-04-19 14:43:24 +0100

    Added FCN for any dataset (not just VOC)

---
+++ examples/fcn/fcnTestGeneric.m
@@ -0,0 +1,293 @@
+function[info] = fcnTestGeneric(varargin)
+% [info] = fcnTestGeneric(varargin)
+%
+% Copyright by Holger Caesar, 2016
+
+% Initial settings
+p = inputParser;
+addParameter(p, 'dataset', VOC2011Dataset());
+addParameter(p, 'modelType', 'fcn8s');
+addParameter(p, 'gpus', 1);
+addParameter(p, 'randSeed', 42);
+addParameter(p, 'expNameAppend', 'test');
+addParameter(p, 'epoch', 50);
+addParameter(p, 'modelFamily', 'matconvnet');
+addParameter(p, 'plotFreq', 30);
+addParameter(p, 'showPlot', false);
+addParameter(p, 'maxImSize', 700);
+addParameter(p, 'mapOutputFolder', ''); % Optional: output predicted labels to file
+parse(p, varargin{:});
+
+dataset = p.Results.dataset;
+modelType = p.Results.modelType;
+gpus = p.Results.gpus;
+randSeed = p.Results.randSeed;
+expNameAppend = p.Results.expNameAppend;
+epoch = p.Results.epoch;
+modelFamily = p.Results.modelFamily;
+plotFreq = p.Results.plotFreq;
+maxImSize = p.Results.maxImSize;
+showPlot = p.Results.showPlot;
+mapOutputFolder = p.Results.mapOutputFolder;
+callArgs = p.Results; %#ok<NASGU>
+
+% experiment and data paths
+global glFeaturesFolder;
+expName = [modelType, prependNotEmpty(expNameAppend, '-')];
+opts.expDir = fullfile(glFeaturesFolder, 'CNN-Models', 'FCN', dataset.name, expName);
+opts.modelPath = fullfile(opts.expDir, sprintf('net-epoch-%d.mat', epoch));
+opts.labelingDir = fullfile(opts.expDir, sprintf('labelings-epoch-%d', epoch));
+
+% experiment setup
+opts.gpus = gpus;
+opts.modelFamily = modelFamily;
+opts.modelType = modelType;
+
+% Fix randomness
+rng(randSeed);
+
+resPath = fullfile(opts.expDir, sprintf('results-epoch-%d.mat', epoch));
+if exist(resPath, 'file')
+    info = load(resPath);
+    return;
+end
+
+% Create dirs
+if ~exist(opts.labelingDir, 'dir'),
+    mkdir(opts.labelingDir);
+end;
+
+% -------------------------------------------------------------------------
+% Setup data
+% -------------------------------------------------------------------------
+
+if strStartsWith(dataset.name, 'VOC'),
+    % Get PASCAL VOC 11/12 segmentation dataset plus Berkeley's additional
+    % segmentations
+    opts.imdbPath = fullfile(opts.expDir, 'imdb.mat');
+    if exist(opts.imdbPath, 'file')
+        imdb = load(opts.imdbPath);
+        
+        % Overwrite imdb dataset to avoid problems with changed paths
+        assert(isequal(dataset.name, imdb.dataset.name));
+        imdb.dataset = dataset;
+    end
+    % Get validation subset
+    val = find(imdb.images.set == 2 & imdb.images.segmentation);
+    
+    bopts.imageNameToLabelMap = @(imageName, imdb) imread(sprintf(imdb.paths.classSegmentation, imageName));
+    bopts.translateLabels = true;
+else
+    % Other datasets
+    % Required fields: imdb.images.name, imdb.paths.image,
+    % imdb.paths.classSegmentation, imdb.dataset, imdb.labelCount
+    imdb.dataset = dataset;
+    [imdb.classes.name, imdb.labelCount] = dataset.getLabelNames();
+    imdb.images.name = dataset.getImageListTst();
+    imdb.paths.image = fullfile(dataset.getImagePath(), sprintf('%%s%s', dataset.imageExt));
+    
+    val = 1:numel(imdb.images.name);
+    
+    bopts.imageNameToLabelMap = @(imageName, imdb) imdb.dataset.getImLabelMap(imageName);
+    bopts.translateLabels = false;
+end;
+
+% -------------------------------------------------------------------------
+% Setup model
+% -------------------------------------------------------------------------
+
+switch opts.modelFamily
+    case 'matconvnet'
+        net = load(opts.modelPath);
+        net = dagnn.DagNN.loadobj(net.net);
+        net.mode = 'test';
+        for name = {'objective', 'accuracy'}
+            % Depending on the level of supervision we might not have an
+            % accuracy layert
+            if ~isnan(net.getLayerIndex(name))
+                net.removeLayer(name);
+            end
+        end
+        net.meta.normalization.averageImage = reshape(net.meta.normalization.rgbMean,1,1,3);
+        predVar = net.getVarIndex('prediction');
+        inputVar = 'input';
+        imageNeedsToBeMultiple = true;
+        
+    case 'ModelZoo'
+        net = dagnn.DagNN.loadobj(load(opts.modelPath));
+        net.mode = 'test';
+        predVar = net.getVarIndex('upscore');
+        inputVar = 'data';
+        imageNeedsToBeMultiple = false;
+        
+    case 'TVG'
+        net = dagnn.DagNN.loadobj(load(opts.modelPath));
+        net.mode = 'test';
+        predVar = net.getVarIndex('coarse');
+        inputVar = 'data';
+        imageNeedsToBeMultiple = false;
+end
+
+if ~isempty(opts.gpus)
+    gpuDevice(opts.gpus(1));
+    net.move('gpu');
+end
+net.mode = 'test';
+
+% -------------------------------------------------------------------------
+% Test
+% -------------------------------------------------------------------------
+
+confusion = zeros(imdb.labelCount);
+
+for i = 1:numel(val)
+    imId = val(i);
+    imageName = imdb.images.name{imId};
+    
+    % Load an image and gt segmentation
+    rgb = double(imdb.dataset.getImage(imageName)) * 255;
+    %   rgb = round(imresize(rgb, imageSize));
+    anno = uint16(bopts.imageNameToLabelMap(imageName, imdb));
+    
+    if bopts.translateLabels,
+        % Before: 255 = ignore, 0 = bkg, 1:labelCount = classes
+        % After : 0 = ignore, 1 = bkg, 2:labelCount+1 = classes
+        anno = mod(anno + 1, 256);
+    end;
+    % 0 = ignore, 1:labelCount = classes
+    
+    % Subtract the mean (color)
+    im = bsxfun(@minus, single(rgb), net.meta.normalization.averageImage);
+    
+    % Workaround: Limit image size to avoid running out of RAM
+    if ~isempty(maxImSize),
+        maxSize = max(size(im, 1), size(im, 2));
+        if maxSize > maxImSize,
+            factor = maxImSize / maxSize;
+            im = imresize(im, factor);
+        end;
+    end;
+    
+    % Soome networks requires the image to be a multiple of 32 pixels
+    if imageNeedsToBeMultiple
+        sz = [size(im, 1), size(im, 2)];
+        sz_ = round(sz / 32) * 32;
+        im_ = imresize(im, sz_);
+    else
+        im_ = im;
+    end
+    
+    if ~isempty(opts.gpus)
+        im_ = gpuArray(im_);
+    end
+    
+    net.eval({inputVar, im_});
+    scores_ = gather(net.vars(predVar).value);
+    [~, pred_] = max(scores_, [], 3);
+    
+    % DEBUG:
+    if true,
+        % Translate label indices if SiftFlow was trained with 34 classes
+        if imdb.labelCount + 1 == net.layers(net.getLayerIndex('fc8')).block.size(4),
+            [~, pred_NoBg] = max(scores_(:, :, 2:end),[],3);
+            pred_ = pred_NoBg;
+        end;
+    end;
+    
+    if imageNeedsToBeMultiple
+        pred = imresize(pred_, size(anno), 'method', 'nearest');
+    else
+        pred = pred_;
+    end
+    
+    % If a folder was specified, output the predicted label maps
+    if ~isempty(mapOutputFolder),
+        outputMap = pred; %#ok<NASGU>
+        outputPath = fullfile(mapOutputFolder, [imageName, '.mat']);
+        save(outputPath, 'outputMap');
+    end;
+    
+    % Accumulate errors
+    ok = anno > 0;
+    confusion = confusion + accumarray([anno(ok), pred(ok)], 1, [imdb.labelCount imdb.labelCount]);
+    
+    % Plots
+    if mod(i - 1, plotFreq) == 0 || i == numel(val)        
+        fprintf('Processing image %d of %d...\n', i, numel(val))
+        
+        % Print segmentation
+        if showPlot,
+            figure(100);
+            clf;
+            displayImage(rgb/255, anno, pred, imdb);
+            drawnow;
+        end;
+        
+        % Save segmentation
+        imPath = fullfile(opts.labelingDir, [imageName '.png']);
+        imwrite(pred, labelColors(imdb), imPath, 'png');
+    end
+end
+
+% Final statistics, remove classes missing in test
+% Note: Printing statistics earlier does not make sense if we remove missing
+% classes
+[info.iu, info.miu, info.pacc, info.macc] = getAccuracies(confusion);
+fprintf('Results without missing classes:\n');
+fprintf('IU ');
+fprintf('%4.1f ', 100 * info.iu);
+fprintf('\n meanIU: %5.2f pixelAcc: %5.2f, meanAcc: %5.2f\n', ...
+    100*info.miu, 100*info.pacc, 100*info.macc);
+
+% Save results
+save(resPath, '-struct', 'info');
+
+% -------------------------------------------------------------------------
+function [IU, meanIU, pixelAccuracy, meanAccuracy] = getAccuracies(confusion)
+% -------------------------------------------------------------------------
+pos = sum(confusion, 2);
+res = sum(confusion, 1)';
+tp = diag(confusion);
+IU = tp ./ max(1, pos + res - tp);
+missing = pos == 0;
+meanIU = mean(IU(~missing));
+pixelAccuracy = sum(tp) / max(1, sum(confusion(:)));
+meanAccuracy = mean(tp(~missing) ./ pos(~missing));
+
+% -------------------------------------------------------------------------
+function displayImage(im, lb, pred, imdb)
+% -------------------------------------------------------------------------
+subplot(2,2,1);
+image(im);
+axis image;
+title('source image');
+
+subplot(2,2,2);
+image(uint8(lb-1));
+axis image;
+title('ground truth')
+
+cmap = labelColors(imdb);
+subplot(2,2,3);
+image(uint8(pred-1));
+axis image;
+title('predicted');
+
+colormap(cmap);
+
+% -------------------------------------------------------------------------
+function cmap = labelColors(imdb)
+% -------------------------------------------------------------------------
+N=imdb.labelCount;
+cmap = zeros(N,3);
+for i=1:N
+    id = i-1; r=0;g=0;b=0;
+    for j=0:7
+        r = bitor(r, bitshift(bitget(id,1),7 - j));
+        g = bitor(g, bitshift(bitget(id,2),7 - j));
+        b = bitor(b, bitshift(bitget(id,3),7 - j));
+        id = bitshift(id,-3);
+    end
+    cmap(i,1)=r; cmap(i,2)=g; cmap(i,3)=b;
+end
+cmap = cmap / 255;
---
+++ examples/fcn/fcnTrainGeneric.m
@@ -0,0 +1,406 @@
+function fcnTrainGeneric(varargin)
+% fcnTrainGeneric(varargin)
+%
+% Train FCN model using MatConvNet.
+%
+% Copyright by Holger Caesar, 2016
+
+% Initial settings
+p = inputParser;
+addParameter(p, 'dataset', SiftFlowDataset());
+addParameter(p, 'modelType', 'fcn16s');
+addParameter(p, 'gpus', 2);
+addParameter(p, 'randSeed', 42);
+addParameter(p, 'expNameAppend', 'test');
+addParameter(p, 'weaklySupervised', false);
+addParameter(p, 'numEpochs', 50);
+addParameter(p, 'useInvFreqWeights', false);
+parse(p, varargin{:});
+
+dataset = p.Results.dataset;
+modelType = p.Results.modelType;
+gpus = p.Results.gpus;
+randSeed = p.Results.randSeed;
+expNameAppend = p.Results.expNameAppend;
+weaklySupervised = p.Results.weaklySupervised;
+numEpochs = p.Results.numEpochs;
+useInvFreqWeights = p.Results.useInvFreqWeights;
+callArgs = p.Results; %#ok<NASGU>
+
+% experiment and data paths
+global glFeaturesFolder;
+mcnDir = filePathRemoveFile(which('fcnTrain'));
+dataRootDir = fullfile(mcnDir, 'data');
+expName = [modelType, prependNotEmpty(expNameAppend, '-')];
+opts.expDir = fullfile(glFeaturesFolder, 'CNN-Models', 'FCN', dataset.name, expName);
+opts.sourceModelPath = fullfile(dataRootDir, 'models', 'imagenet-vgg-verydeep-16.mat');
+logFilePath = fullfile(opts.expDir, 'log.txt');
+
+% training options (SGD)
+opts.train.batchSize = 20;
+opts.train.numSubBatches = 10;
+opts.train.continue = true;
+opts.train.gpus = gpus;
+opts.train.prefetch = true;
+opts.train.expDir = opts.expDir;
+opts.train.numEpochs = numEpochs;
+opts.train.learningRate = 1e-4 * ones(1, opts.train.numEpochs);
+opts.modelType = modelType;
+
+% Fix randomness
+rng(randSeed);
+
+% Create folders
+if ~exist(opts.expDir, 'dir'),
+    mkdir(opts.expDir);
+end;
+
+% Setup logfile
+diary(logFilePath);
+
+% -------------------------------------------------------------------------
+% Setup data
+% -------------------------------------------------------------------------
+
+opts.imdbPath = fullfile(opts.expDir, 'imdb.mat');
+if exist(opts.imdbPath, 'file')
+    imdb = load(opts.imdbPath);
+else
+    % Dataset-specific imdb creation
+    if strStartsWith(dataset.name, 'VOC'),
+        % Get PASCAL VOC 12 segmentation dataset plus Berkeley's additional
+        % segmentations
+        opts.vocAdditionalSegmentations = true;
+        opts.vocEdition = '11';
+        opts.dataDir = fullfile(dataRootDir, dataset.name);
+        imdb = vocSetup('dataDir', opts.dataDir, ...
+            'edition', opts.vocEdition, ...
+            'includeTest', false, ...
+            'includeSegmentation', true, ...
+            'includeDetection', false);
+        if opts.vocAdditionalSegmentations
+            imdb = vocSetupAdditionalSegmentations(imdb, 'dataDir', opts.dataDir);
+        end
+        labelCount = 21;
+        
+    else
+        % Imdb must have the following fields:
+        % imdb.images.name, imdb.classes.name, imdb.labelCount, imdb.dataset
+        
+        % Get labels and image path
+        [imdb.classes.name, labelCount] = dataset.getLabelNames();
+        imdb.paths.image = fullfile(dataset.getImagePath(), sprintf('%%s%s', dataset.imageExt));
+    end;
+    
+    % Dataset-independent imdb fields
+    imdb.dataset = dataset;
+    imdb.labelCount = labelCount;
+    imdb.weaklySupervised = weaklySupervised;
+    imdb.useInvFreqWeights = useInvFreqWeights;
+    
+    % Save IMDB
+    save(opts.imdbPath, '-struct', 'imdb');
+end;
+
+if strStartsWith(dataset.name, 'VOC'),
+    % Get training and test/validation subsets
+    train = find(imdb.images.set == 1 & imdb.images.segmentation);
+    val = find(imdb.images.set == 2 & imdb.images.segmentation);
+    
+    % Get mean image
+    % Get dataset statistics
+    opts.imdbStatsPath = fullfile(opts.expDir, 'imdbStats.mat');
+    if exist(opts.imdbStatsPath, 'file')
+        stats = load(opts.imdbStatsPath);
+    else
+        stats = getDatasetStatistics(imdb);
+        save(opts.imdbStatsPath, '-struct', 'stats');
+    end
+    
+    % Batch options
+    rgbMean = stats.rgbMean;
+    translateLabels = true;
+else
+    % Get trn+val images
+    [imageList, imageCount] = dataset.getImageListTrn();
+    
+    % Remove images without labels
+    if isa(dataset, 'EdiStuffDataset') || isa(dataset, 'EdiStuffSubsetDataset'),
+        missingImageIndices = dataset.getMissingImageIndices('train');
+        imageList(missingImageIndices) = [];
+        imageCount = numel(imageList);
+    end;
+    
+    % Get training and test/validation subsets
+    perm = randperm(imageCount)';
+    split = round(0.9 * imageCount);
+    train = perm(1:split);
+    val = perm(split+1:end);
+    imdb.images.name = imageList;
+    
+    rgbMean = imdb.dataset.getMeanColor();
+    translateLabels = false;
+end;
+
+
+% -------------------------------------------------------------------------
+% Setup model
+% -------------------------------------------------------------------------
+
+% Get initial model from VGG-VD-16
+net = fcnInitializeModelGeneric(imdb.labelCount, 'sourceModelPath', opts.sourceModelPath);
+if any(strcmp(opts.modelType, {'fcn16s', 'fcn8s'}))
+    % upgrade model to FCN16s
+    net = fcnInitializeModel16sGeneric(imdb.labelCount, net);
+end
+if strcmp(opts.modelType, 'fcn8s')
+    % upgrade model fto FCN8s
+    net = fcnInitializeModel8sGeneric(imdb.labelCount, net);
+end
+net.meta.normalization.rgbMean = rgbMean;
+net.meta.classes = imdb.classes.name;
+
+
+% Replace loss by weighted loss
+objIdx = net.getLayerIndex('objective');
+assert(strcmp(net.layers(objIdx).block.loss, 'softmaxlog'));
+objInputs = net.layers(objIdx).inputs;
+objOutputs = net.layers(objIdx).outputs;
+net.removeLayer('objective');
+net.addLayer('objective', SegmentationLossWeighted(), objInputs, objOutputs, {});
+
+if weaklySupervised,
+    % Insert a labelpresence layer
+    predVarIdx = net.getVarIndex('prediction');
+    predLastLayerIdx = cellfun(@(x) ismember(predVarIdx, x), {net.layers.outputIndexes});
+    predLastLayerName = net.layers(predLastLayerIdx).name;
+    
+    labelPresenceBlock = dagnn.SegmentationLabelPresence();
+    net.insertLayer(predLastLayerName, 'objective', 'labelpresence', labelPresenceBlock, {}, {'predictionImage'}, {});
+    net.setLayerInputs('labelpresence', {'prediction', 'labelImages'});
+    
+    % Update inputs of loss layer
+    objInputs = {'predictionImage', 'labelsImageFlat'};
+    net.setLayerInputs('objective', objInputs);
+    
+    % Remove accuracy layer if no pixel-level labels exist
+    if ~imdb.dataset.annotation.hasPixelLabels,
+        net.removeLayer('accuracy');
+    end;
+end;
+
+% Replace accuracy layer with 21 classes by flexible accuracy layer
+if imdb.dataset.annotation.hasPixelLabels
+    accIdx = net.getLayerIndex('accuracy');
+    accLayer = net.layers(accIdx);
+    accInputs = accLayer.inputs;
+    accOutputs = accLayer.outputs;
+    accBlock = SegmentationAccuracyFlexible('labelCount', imdb.labelCount);
+    net.removeLayer('accuracy');
+    net.addLayer('accuracy', accBlock, accInputs, accOutputs, {});
+end;
+
+% Extract inverse class frequencies from dataset
+if useInvFreqWeights,
+    
+    
+    if weaklySupervised,
+        classWeights = imdb.dataset.getLabelImFreqs('train');
+    else
+        classWeights = imdb.dataset.getLabelPixelFreqs('train');
+        
+        % Add weight as input of loss layer
+        objInputs = [net.layers(net.getLayerIndex('objective')).inputs, {'pixelWeights'}];
+        net.setLayerInputs('objective', objInputs);
+    end;
+    
+    % Inv freq and normalize classWeights
+    classWeights = classWeights ./ sum(classWeights);
+    nonEmpty = classWeights ~= 0;
+    classWeights(nonEmpty) = 1 ./ classWeights(nonEmpty);
+    classWeights = classWeights ./ sum(classWeights);
+    assert(~any(isnan(classWeights)));
+else
+    classWeights = [];
+end;
+
+% -------------------------------------------------------------------------
+% Train
+% -------------------------------------------------------------------------
+
+% Setup data fetching options
+bopts.labelStride = 1;
+bopts.labelOffset = 1;
+bopts.classWeights = classWeights;
+bopts.rgbMean = rgbMean;
+bopts.useGpu = numel(opts.train.gpus) > 0;
+if ~strStartsWith(imdb.dataset.name, 'VOC'),
+    bopts.imageNameToLabelMap = @(imageName, imdb) imdb.dataset.getImLabelMap(imageName);
+end;
+bopts.translateLabels = translateLabels;
+
+% Save important settings
+settingsPath = fullfile(opts.expDir, 'settings.mat');
+save(settingsPath, 'callArgs', 'opts', 'bopts');
+
+% Launch SGD
+[~, stats] = cnn_train_dag(net, imdb, getBatchWrapper(bopts), opts.train, ...
+    'train', train, ...
+    'val', val); %#ok<ASGLU>
+
+% Output stats
+statsPath = fullfile(opts.expDir, 'stats.mat');
+save(statsPath, 'stats');
+
+% -------------------------------------------------------------------------
+function fn = getBatchWrapper(opts)
+% -------------------------------------------------------------------------
+fn = @(imdb,batch) getBatch(imdb,batch,opts,'prefetch',nargout==0);
+
+function y = getBatch(imdb, images, varargin)
+% GET_BATCH  Load, preprocess, and pack images for CNN evaluation
+
+opts.imageSize = [512, 512] - 128;
+opts.numAugments = 1;
+opts.transformation = 'none';
+opts.rgbMean = [];
+opts.rgbVariance = zeros(0,3,'single');
+opts.labelStride = 1;
+opts.labelOffset = 0;
+opts.classWeights = [];
+opts.interpolation = 'bilinear';
+opts.prefetch = false;
+opts.useGpu = false;
+opts.imageNameToLabelMap = @(imageName, imdb) imread(sprintf(imdb.paths.classSegmentation, imageName));
+opts.translateLabels = true;
+opts = vl_argparse(opts, varargin);
+
+if opts.prefetch
+    % to be implemented
+    y = {};
+    return;
+end
+
+if ~isempty(opts.rgbVariance) && isempty(opts.rgbMean)
+    opts.rgbMean = single([128;128;128]);
+end
+if ~isempty(opts.rgbMean)
+    opts.rgbMean = reshape(opts.rgbMean, [1 1 3]);
+end
+
+% space for images
+ims = zeros(opts.imageSize(1), opts.imageSize(2), 3, ...
+    numel(images)*opts.numAugments, 'single');
+
+% space for labels
+lx = opts.labelOffset : opts.labelStride : opts.imageSize(2);
+ly = opts.labelOffset : opts.labelStride : opts.imageSize(1);
+labels = zeros(numel(ly), numel(lx), 1, numel(images)*opts.numAugments, 'double'); % must be double for to avoid numerical precision errors in vl_nnloss, when using many classes
+if imdb.weaklySupervised,
+    labelsImage = cell(size(labels, 4), 1);
+end;
+
+si = 1;
+
+for i=1:numel(images)
+    
+    % acquire image
+    imageName = imdb.images.name{images(i)};
+    rgb = double(imdb.dataset.getImage(imageName)) * 255;
+    if size(rgb,3) == 1
+        rgb = cat(3, rgb, rgb, rgb);
+    end
+    
+    % acquire pixel-level GT
+    if imdb.dataset.annotation.hasPixelLabels,
+        anno = uint16(opts.imageNameToLabelMap(imageName, imdb));
+        if opts.translateLabels,
+            % Before: 255 = ignore, 0 = bkg, 1:labelCount = classes
+            % After : 0 = ignore, 1 = bkg, 2:labelCount+1 = classes
+            anno = mod(anno + 1, 256);
+        end;
+        % 0 = ignore, 1:labelCount = classes
+    end;
+    
+    % crop & flip
+    h = size(rgb,1);
+    w = size(rgb,2);
+    for ai = 1:opts.numAugments
+        sz = opts.imageSize(1:2);
+        scale = max(h/sz(1), w/sz(2));
+        scale = scale .* (1 + (rand(1)-.5)/5);
+        
+        sy = round(scale * ((1:sz(1)) - sz(1)/2) + h/2);
+        sx = round(scale * ((1:sz(2)) - sz(2)/2) + w/2);
+        if rand > 0.5, sx = fliplr(sx); end
+        
+        okx = find(1 <= sx & sx <= w);
+        oky = find(1 <= sy & sy <= h);
+        if ~isempty(opts.rgbMean)
+            ims(oky, okx, :, si) = bsxfun(@minus, rgb(sy(oky), sx(okx), :), opts.rgbMean);
+        else
+            ims(oky, okx, :, si) = rgb(sy(oky), sx(okx),:);
+        end
+        
+        % Fully supervised: Get pixel level labels
+        if imdb.dataset.annotation.hasPixelLabels,
+            tlabels = zeros(sz(1), sz(2), 'double');
+            tlabels(oky,okx) = anno(sy(oky), sx(okx));
+            tlabels = single(tlabels(ly,lx));
+            labels(:,:,1,si) = tlabels; % 0: ignore
+        end;
+        
+        % Weakly supervised: extract image-level labels
+        if imdb.weaklySupervised,
+            if imdb.dataset.annotation.hasPixelLabels,
+                curLabelsImage = unique(anno);
+                curLabelsImage(curLabelsImage == 0) = [];
+                curLabelsImage = curLabelsImage(:);
+                labelsImage{si} = single(curLabelsImage);
+            else
+                curLabelsImage = imdb.dataset.getImLabelInds(imageName);
+                
+                labelsImage{si} = curLabelsImage;
+            end;
+        end;
+        
+        si = si + 1;
+    end
+end
+
+% Create instance weighting
+if ~isempty(opts.classWeights),
+    classWeightsPad = [0; opts.classWeights(:)];
+    
+    if imdb.weaklySupervised,
+        warning('Inv freq. weighting not implemented for WS!');
+    else
+        pixelWeights = classWeightsPad(labels+1);
+        
+        % Make sure mass of the image does not change
+        targetMasses = sum(sum(labels > 0, 1), 2);
+        curMasses = sum(sum(pixelWeights, 1), 2);
+        divisor = curMasses ./ targetMasses;
+        nonZero = targetMasses ~= 0;
+        pixelWeights(:, :, :, nonZero) = bsxfun(@rdivide, pixelWeights(:, :, :, nonZero), divisor(nonZero));
+        % assert(all(abs(sum(sum(pixelWeights, 1), 2) - targetMasses) < 1e-9))
+    end;
+end;
+
+% Move image to GPU
+if opts.useGpu
+    ims = gpuArray(ims);
+end
+
+% Create outputs
+y = {'input', ims};
+if imdb.dataset.annotation.hasPixelLabels,
+    y = [y, {'label', labels}];
+end;
+if imdb.weaklySupervised,
+    labelsImageFlat = reshape(cell2mat(labelsImage), 1, 1, 1, []);
+    y = [y, {'labelImages', labelsImage, 'labelsImageFlat', labelsImageFlat}];
+end;
+if ~isempty(pixelWeights),
+    y = [y, {'pixelWeights', pixelWeights}];
+end;
