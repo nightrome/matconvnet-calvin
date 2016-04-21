759d9656c64315885e564844dac2bf63c21fd507
Holger Caesar <holger.caesar@ed.ac.uk>
2016-04-21 16:40:38 +0100

    Completely reworked image-level loss layers

--- matlab/+dagnn/SegmentationLossImage.m
+++ matlab/+dagnn/SegmentationLossImage.m
@@ -20,6 +20,9 @@ classdef SegmentationLossImage < dagnn.Loss
     properties (Transient)
         mask
         scoresImage
+        instanceWeights
+        isPresent
+        softmax
     end
     
     methods
@@ -31,8 +34,8 @@ classdef SegmentationLossImage < dagnn.Loss
             labelsImageCell = inputs{2};
             classWeights = inputs{3};
             labelCount = size(scoresMap, 3);
+            imageCount = size(scoresMap, 4);
             sampleCount = labelCount * imageCount;
-            imageCount = numel(labelsImageCell);
             labelsDummy = repmat(1:labelCount, [1, imageCount]);
             labelsDummy = reshape(labelsDummy, 1, 1, 1, []);
 
@@ -47,7 +50,7 @@ classdef SegmentationLossImage < dagnn.Loss
             if true
                 % Count total number of samples and init scores
                 obj.scoresImage = nan(1, 1, labelCount, sampleCount, 'single');
-                obj.mask = nan(labelCount, labelCount); % contains the coordinates of the pixel with highest score per class
+                obj.mask = nan(sampleCount, 1); % contains the coordinates of the pixel with highest score per class
                 
                 % Process each image/crop separately
                 for imageIdx = 1 : imageCount
@@ -55,10 +58,12 @@ classdef SegmentationLossImage < dagnn.Loss
                     labelMax = max(max(scoresMap(:, :, :, imageIdx), [], 1), [], 2);
                     
                     for labelIdx = 1 : labelCount
-                        [y, x] = find(scoresMap(:, :, labelIdx, imageIdx) == labelMax(labelIdx), 1); % always take first pix with max score
                         offset = (imageIdx-1) * labelCount;
-                        obj.scoresImage(:, :, :, offset+labelIdx) = scoresMap(y, x, :, imageIdx);
-                        obj.mask(:, labelIdx) = y + (x - 1) * size(scoresMap, 1);
+                        sampleIdx = offset + labelIdx;
+                        
+                        [y, x] = find(scoresMap(:, :, labelIdx, imageIdx) == labelMax(labelIdx), 1); % always take first pix with max score
+                        obj.scoresImage(:, :, :, sampleIdx) = scoresMap(y, x, :, imageIdx);
+                        obj.mask(sampleIdx, 1) = y + (x - 1) * size(scoresMap, 1);
                     end
                 end
             end
@@ -86,83 +91,51 @@ classdef SegmentationLossImage < dagnn.Loss
                     presentInds{imageIdx} = labelsImageCell{imageIdx} + (imageIdx-1) * labelCount;
                 end;
                 presentInds = cell2mat(presentInds);
-                isPresent = ismember(1:sampleCount, presentInds)';
+                obj.isPresent = ismember(1:sampleCount, presentInds)';
                 
                 % Compute loss
                 % To simplify things we remove the subtraction of Xmax from
                 % X, which is supposed to reduce numerical problems
-                ex = exp(X);
-                sumExp = sum(ex, 3);
-                softmax = bsxfun(@rdivide, ex, sumExp);
-                softmax(:, :, :, ~isPresent) = 1 - softmax(:, :, :, ~isPresent);
-                t = -log(softmax(ci));
-                instanceWeights = ones(1, 1, 1, sampleCount);
+                ex = exp(bsxfun(@minus, X, max(X, [], 3)));
+                sumEx = sum(ex, 3);
+                obj.softmax = bsxfun(@rdivide, ex, sumEx);
+                obj.softmax(:, :, :, ~obj.isPresent) = 1 - obj.softmax(:, :, :, ~obj.isPresent);
+                obj.softmax = max(obj.softmax, 1e-10);
+                t = -log(obj.softmax(ci));
+                obj.instanceWeights = ones(1, 1, 1, sampleCount);
                 
                 % Weight per class
                 if ~isempty(classWeights)
-                    instanceWeights = instanceWeights .* classWeights(labelsDummy);
+                    obj.instanceWeights = obj.instanceWeights .* classWeights(labelsDummy);
                 end
                 
                 % Renormalize present labels per image
                 sampleImage = repmatEach(1:imageCount, labelCount);
                 presentWeight = 1 / (1 + obj.useAbsent); % give all or half of the weight to presence
                 for imageIdx = 1 : imageCount
-                    sel = sampleImage == imageIdx & isPresent;
-                    instanceWeights(sel) = instanceWeights(sel) ./ (sum(instanceWeights(sel)) / presentWeight);
+                    sel = sampleImage == imageIdx & obj.isPresent;
+                    obj.instanceWeights(sel) = obj.instanceWeights(sel) ./ (sum(obj.instanceWeights(sel)) / presentWeight);
                 end
                 
                 % Renormalize or disable absent labels per image
                 for imageIdx = 1 : imageCount,
-                    sel = sampleImage == imageIdx & ~isPresent;
+                    sel = sampleImage == imageIdx & ~obj.isPresent;
                     
                     if obj.useAbsent
-                        instanceWeights(sel) = instanceWeights(sel) ./ (sum(instanceWeights(sel)) / presentWeight);
+                        obj.instanceWeights(sel) = obj.instanceWeights(sel) ./ (sum(obj.instanceWeights(sel)) / presentWeight);
                     else
-                        instanceWeights(sel) = 0;
+                        obj.instanceWeights(sel) = 0;
                     end
                 end
                 
-                loss = sum(t .* instanceWeights);
-                
-%                 if true
-%                     presentInds = cell(imageCount, 1);
-%                     for imageIdx = 1 : imageCount,
-%                         presentInds{imageIdx} = labelsImageCell{imageIdx} + (imageIdx-1) * labelCount;
-%                     end;
-%                     presentInds = cell2mat(presentInds);
-%                     presentT = log(sum(ex(:, :, :, presentInds), 3)) - X(ci(:, :, :, presentInds));
-%                     
-%                     presentWeights = ones(size(presentT)) / numel(presentT);
-%                     if ~isempty(classWeights)
-%                         presentWeights = presentWeights .* classWeights(presentInds);
-%                     end
-%                     presentLoss = sum(presentT .* presentWeights);
-%                     loss = presentLoss;
-%                 end
-%                 
-%                 if obj.useAbsent
-%                     absentInds = cell(imageCount, 1);
-%                     for imageIdx = 1 : imageCount,
-%                         absentInds{imageIdx} = labelsImageCell{imageIdx} + (imageIdx-1) * labelCount;
-%                     end;
-%                     absentInds = cell2mat(absentInds);
-%                     absentSumExp = sumExp(:, :, :, absentInds);
-%                     absentT = log(absentSumExp) - log(absentSumExp - ex(ci(:, :, :, absentInds)));
-%                     
-%                     absentWeights = ones(size(absentT)) / numel(absentT);
-%                     if ~isempty(classWeights)
-%                         absentWeights = absentWeights .* classWeights(absentInds);
-%                     end
-%                     absentLoss = sum(absentT .* absentWeights);
-%                     loss = (loss + absentLoss) / 2;
-%                 end
+                loss = sum(t .* obj.instanceWeights);
             end;
             
             %%%% Assign outputs
             outputs{1} = loss;
             
             % Update statistics
-            assert(gather(~isnan(loss)));
+            assert(~isnan(loss) && ~isinf(loss));
             n = obj.numAveraged;
             m = n + imageCount;
             obj.average = (n * obj.average + double(gather(loss))) / m;
@@ -174,23 +147,21 @@ classdef SegmentationLossImage < dagnn.Loss
             %%%% Get inputs
             assert(numel(inputs) == 3);
             scoresMap = inputs{1};
-            labelsImageCell = inputs{2};
-            classWeights = inputs{3};
+            imageSizeY = size(scoresMap, 1);
             labelCount = size(scoresMap, 3);
-            imageCount = numel(labelsImageCell);
+            imageCount = size(scoresMap, 4);
             labelsDummy = repmat(1:labelCount, [1, imageCount]);
             labelsDummy = reshape(labelsDummy, 1, 1, 1, []);
             
             assert(numel(derOutputs) == 1);
-            dzdLoss = derOutputs{1};
+            dzdOutput = derOutputs{1};
             
             %%%% Loss derivatives
-            
-            X = obj.scoresImage;
+            X = obj.softmax;
             c = labelsDummy;
             
-            inputSize = [size(X,1) size(X,2) size(X,3) size(X,4)];
-            labelSize = [size(c,1) size(c,2) size(c,3) size(c,4)];
+            inputSize = [size(X, 1), size(X, 2), size(X, 3), size(X, 4)];
+            labelSize = [size(c, 1), size(c, 2), size(c, 3), size(c, 4)];
             assert(isequal(labelSize(1:2), inputSize(1:2)));
             assert(labelSize(4) == inputSize(4));
             
@@ -199,46 +170,47 @@ classdef SegmentationLossImage < dagnn.Loss
             numPixels = numPixelsPerImage * inputSize(4);
             imageVolume = numPixelsPerImage * inputSize(3);
             
-            n = reshape(0:numPixels-1,labelSize);
+            n = reshape(0:numPixels-1, labelSize);
             offset = 1 + mod(n, numPixelsPerImage) + ...
                 imageVolume * fix(n / numPixelsPerImage);
             ci = offset + numPixelsPerImage * max(c - 1,0);
             
-            % Compute gradients
-%             dzdLoss = dzdLoss * instanceWeights;
-                        
-            Xmax = max(X,[],3);
-            ex = exp(bsxfun(@minus, X, Xmax));
-            dzdImage = bsxfun(@rdivide, ex, sum(ex,3));
-            dzdImage(ci) = dzdImage(ci) - 1;
-            dzdImage = bsxfun(@times, dzdLoss, dzdImage);
+            % Weight gradients per instance
+            dzdOutput = dzdOutput * obj.instanceWeights;
+            
+            % Compute gradients for log-loss
+            dzdLoss = zeros(size(X), 'like', X);
+            dzdLoss(ci) = - dzdOutput ./ max(X(ci), 1e-8);
+            
+            % Compute gradients for softmax
+            X = obj.scoresImage;
+            ex = exp(bsxfun(@minus, X, max(X, [], 3)));
+            sumEx = sum(ex, 3);
+            softmaxDef = bsxfun(@rdivide, ex, sumEx);
+            dzdImage = softmaxDef .* bsxfun(@minus, dzdLoss, sum(dzdLoss .* softmaxDef, 3));
             
             %%%% Map gradients from image to pixel-level
-            imageSizeY = size(scoresMap, 1);
-            imageSizeX = size(scoresMap, 2);
-            labelCount = size(scoresMap, 3);
-            imageCount = numel(labelsImageCell);
-            
-            % Map Image gradients to RP+GT gradients
-            dzdxSamples = segmentationLabelPresence_backward(imageSizeY, imageSizeX, obj.mask, dzdImage);
-            
-            % Sum the per-sammple gradients of each image
-            dzdx = nan(imageSizeY, imageSizeX, labelCount, imageCount, 'single');
-            totalSampleOffset = 0;
-            for imageIdx = 1 : imageCount,
-                samples = totalSampleOffset + 1 : totalSampleOffset + numel(labelsImageCell{imageIdx});
-                dzdx(:, :, :, imageIdx) = nansum(dzdxSamples(:, :, :, samples), 4);
-                totalSampleOffset = totalSampleOffset + numel(labelsImageCell{imageIdx});
-            end;
+            dzdxMap = zeros(size(scoresMap), 'single');
+            for imageIdx = 1 : imageCount
+                for labelIdx = 1 : labelCount
+                    offset = (imageIdx-1) * labelCount;
+                    sampleIdx = offset + labelIdx;
+                    pos = obj.mask(sampleIdx, 1);
+                    x = 1 + floor((pos-1) / imageSizeY);
+                    y = pos - (x-1) * imageSizeY;
+                    
+                    dzdxMap(y, x, :, imageIdx) = dzdxMap(y, x, :, imageIdx) + dzdImage(1, 1, :, sampleIdx);
+                end
+            end
             
             % Move outputs to GPU if necessary
             gpuMode = isa(inputs{1}, 'gpuArray');
             if gpuMode
-                dzdx = gpuArray(dzdx);
+                dzdxMap = gpuArray(dzdxMap);
             end
             
             %%%% Assign outputs
-            derInputs{1} = dzdx;
+            derInputs{1} = dzdxMap;
             derInputs{2} = [];
             derInputs{3} = [];
             derInputs{4} = [];
