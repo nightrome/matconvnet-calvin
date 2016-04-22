df3ca49a6df95cd9a5645cff7e33ba3edf78ae72
Holger Caesar <holger.caesar@ed.ac.uk>
2016-04-22 12:37:25 +0100

    Reworked SegmentationLossImage to have softmax before pixel to image mapping

--- matlab/+dagnn/SegmentationLossImage.m
+++ matlab/+dagnn/SegmentationLossImage.m
@@ -5,6 +5,9 @@ classdef SegmentationLossImage < dagnn.Loss
     % "What's the point: Semantic segmentation with point supervision" by
     % Russakovsky et al., arxiv 2015
     %
+    % scoresMap -> scoresMapSoftmax -> scoresImageSoftmax ->
+    % scoresImageSoftmaxAbs -> loss
+    %
     % Inputs: scoresMap, labelsImage, classWeights
     % Outputs: loss
     %
@@ -19,10 +22,10 @@ classdef SegmentationLossImage < dagnn.Loss
     
     properties (Transient)
         mask
-        scoresImage
+        scoresMapSoftmax
+        scoresImageSoftmaxAbs
         instanceWeights
         isPresent
-        softmax
     end
     
     methods
@@ -38,6 +41,7 @@ classdef SegmentationLossImage < dagnn.Loss
             sampleCount = labelCount * imageCount;
             labelsDummy = repmat(1:labelCount, [1, imageCount]);
             labelsDummy = reshape(labelsDummy, 1, 1, 1, []);
+            assert(~any(cellfun(@(x) isempty(x), labelsImageCell)));
 
             %%%% Pixel to image mapping
             % Move to CPU
@@ -46,9 +50,20 @@ classdef SegmentationLossImage < dagnn.Loss
                 scoresMap = gather(scoresMap);
             end
             
+            % Softmax pixel-level scores
+            if true
+                % Compute loss
+                % To simplify things we remove the subtraction of Xmax from
+                % X, which is supposed to reduce numerical problems
+                X = scoresMap;
+                ex = exp(bsxfun(@minus, X, max(X, [], 3)));
+                sumEx = sum(ex, 3);
+                obj.scoresMapSoftmax = bsxfun(@rdivide, ex, sumEx);
+            end
+            
             if true
                 % Count total number of samples and init scores
-                obj.scoresImage = nan(1, 1, labelCount, sampleCount, 'single');
+                scoresImageSoftmax = nan(1, 1, labelCount, sampleCount, 'single');
                 obj.mask = nan(sampleCount, 1); % contains the coordinates of the pixel with highest score per class
                 
                 % Process each image/crop separately % very slow (!!)
@@ -58,19 +73,34 @@ classdef SegmentationLossImage < dagnn.Loss
                     for labelIdx = 1 : labelCount
                         sampleIdx = offset + labelIdx;
                         
-                        s = scoresMap(:, :, labelIdx, imageIdx);
+                        s = obj.scoresMapSoftmax(:, :, labelIdx, imageIdx);
                         [~, ind] = max(s(:)); % always take first pix with max score
-                        x = 1 + floor((ind-1) / size(scoresMap, 1));
-                        y = ind - (x-1) * size(scoresMap, 1);
-                        obj.scoresImage(1, 1, :, sampleIdx) = scoresMap(y, x, :, imageIdx);
-                        obj.mask(sampleIdx, 1) = y + (x - 1) * size(scoresMap, 1);
+                        x = 1 + floor((ind-1) / size(obj.scoresMapSoftmax, 1));
+                        y = ind - (x-1) * size(obj.scoresMapSoftmax, 1);
+                        scoresImageSoftmax(1, 1, :, sampleIdx) = obj.scoresMapSoftmax(y, x, :, imageIdx);
+                        obj.mask(sampleIdx, 1) = y + (x - 1) * size(obj.scoresMapSoftmax, 1);
                     end
                 end
             end
             
             %%% Loss function from vl_nnloss
             if true,
-                X = obj.scoresImage;
+                % Get presence/absence info
+                presentInds = cell(imageCount, 1);
+                for imageIdx = 1 : imageCount,
+                    presentInds{imageIdx} = labelsImageCell{imageIdx} + (imageIdx-1) * labelCount;
+                end;
+                presentInds = cell2mat(presentInds);
+                obj.isPresent = ismember(1:sampleCount, presentInds)';
+                
+                % Take 1 - p for classes missing in the image
+                obj.scoresImageSoftmaxAbs = scoresImageSoftmax;
+                if obj.useAbsent
+                    obj.scoresImageSoftmaxAbs(:, :, :, ~obj.isPresent) = 1 - obj.scoresImageSoftmaxAbs(:, :, :, ~obj.isPresent);
+                    obj.scoresImageSoftmaxAbs = max(obj.scoresImageSoftmaxAbs, 1e-6);
+                end
+                
+                X = obj.scoresImageSoftmaxAbs;
                 c = labelsDummy;
                 
                 % from category labels to indexes
@@ -85,26 +115,11 @@ classdef SegmentationLossImage < dagnn.Loss
                     imageVolume * fix(n / numPixelsPerImage);
                 ci = offset + numPixelsPerImage * max(c - 1, 0);
                 
-                % Get presence/absence info
-                presentInds = cell(imageCount, 1);
-                for imageIdx = 1 : imageCount,
-                    presentInds{imageIdx} = labelsImageCell{imageIdx} + (imageIdx-1) * labelCount;
-                end;
-                presentInds = cell2mat(presentInds);
-                obj.isPresent = ismember(1:sampleCount, presentInds)';
-                
                 % Compute loss
-                % To simplify things we remove the subtraction of Xmax from
-                % X, which is supposed to reduce numerical problems
-                ex = exp(bsxfun(@minus, X, max(X, [], 3)));
-                sumEx = sum(ex, 3);
-                obj.softmax = bsxfun(@rdivide, ex, sumEx);
-                obj.softmax(:, :, :, ~obj.isPresent) = 1 - obj.softmax(:, :, :, ~obj.isPresent);
-                obj.softmax = max(obj.softmax, 1e-10);
-                t = -log(obj.softmax(ci));
-                obj.instanceWeights = ones(1, 1, 1, sampleCount);
+                t = -log(X(ci));
                 
                 % Weight per class
+                obj.instanceWeights = ones(1, 1, 1, sampleCount);
                 if ~isempty(classWeights)
                     obj.instanceWeights = obj.instanceWeights .* classWeights(labelsDummy);
                 end
@@ -169,40 +184,36 @@ classdef SegmentationLossImage < dagnn.Loss
             dzdOutput = derOutputs{1};
             
             %%%% Loss derivatives
-            X = obj.softmax;
-            c = labelsDummy;
+            %%% Output to image-level
+            if true,
+                X = obj.scoresImageSoftmaxAbs;
+                c = labelsDummy;
+                
+                inputSize = [size(X, 1), size(X, 2), size(X, 3), size(X, 4)];
+                labelSize = [size(c, 1), size(c, 2), size(c, 3), size(c, 4)];
+                assert(isequal(labelSize(1:2), inputSize(1:2)));
+                assert(labelSize(4) == inputSize(4));
+                
+                % from category labels to indexes
+                numPixelsPerImage = prod(inputSize(1:2));
+                numPixels = numPixelsPerImage * inputSize(4);
+                imageVolume = numPixelsPerImage * inputSize(3);
+                
+                n = reshape(0:numPixels-1, labelSize);
+                offset = 1 + mod(n, numPixelsPerImage) + ...
+                    imageVolume * fix(n / numPixelsPerImage);
+                ci = offset + numPixelsPerImage * max(c - 1,0);
+                
+                % Weight gradients per instance
+                dzdOutput = dzdOutput * obj.instanceWeights;
+                
+                % Compute gradients for log-loss
+                dzdImageSoftmax = zeros(size(X), 'like', X);
+                dzdImageSoftmax(ci) = - dzdOutput ./ X(ci);
+            end;
             
-            inputSize = [size(X, 1), size(X, 2), size(X, 3), size(X, 4)];
-            labelSize = [size(c, 1), size(c, 2), size(c, 3), size(c, 4)];
-            assert(isequal(labelSize(1:2), inputSize(1:2)));
-            assert(labelSize(4) == inputSize(4));
-            
-            % from category labels to indexes
-            numPixelsPerImage = prod(inputSize(1:2));
-            numPixels = numPixelsPerImage * inputSize(4);
-            imageVolume = numPixelsPerImage * inputSize(3);
-            
-            n = reshape(0:numPixels-1, labelSize);
-            offset = 1 + mod(n, numPixelsPerImage) + ...
-                imageVolume * fix(n / numPixelsPerImage);
-            ci = offset + numPixelsPerImage * max(c - 1,0);
-            
-            % Weight gradients per instance
-            dzdOutput = dzdOutput * obj.instanceWeights;
-            
-            % Compute gradients for log-loss
-            dzdLoss = zeros(size(X), 'like', X);
-            dzdLoss(ci) = - dzdOutput ./ max(X(ci), 1e-8);
-            
-            % Compute gradients for softmax
-            X = obj.scoresImage;
-            ex = exp(bsxfun(@minus, X, max(X, [], 3)));
-            sumEx = sum(ex, 3);
-            softmaxDef = bsxfun(@rdivide, ex, sumEx);
-            dzdImage = softmaxDef .* bsxfun(@minus, dzdLoss, sum(dzdLoss .* softmaxDef, 3));
-            
-            %%%% Map gradients from image to pixel-level
-            dzdxMap = zeros(size(scoresMap), 'single');
+            %%% Image to pixel-level
+            dzdxMapSoftmax = zeros(size(scoresMap), 'single');
             for imageIdx = 1 : imageCount
                 for labelIdx = 1 : labelCount
                     offset = (imageIdx-1) * labelCount;
@@ -211,9 +222,12 @@ classdef SegmentationLossImage < dagnn.Loss
                     x = 1 + floor((pos-1) / imageSizeY);
                     y = pos - (x-1) * imageSizeY;
                     
-                    dzdxMap(y, x, :, imageIdx) = dzdxMap(y, x, :, imageIdx) + dzdImage(1, 1, :, sampleIdx);
+                    dzdxMapSoftmax(y, x, :, imageIdx) = dzdxMapSoftmax(y, x, :, imageIdx) + dzdImageSoftmax(1, 1, :, sampleIdx);
                 end
             end
+            
+            %%% Softmax to non-softmax
+            dzdxMap = obj.scoresMapSoftmax .* bsxfun(@minus, dzdxMapSoftmax, sum(dzdxMapSoftmax .* obj.scoresMapSoftmax, 3));
             
             % Move outputs to GPU if necessary
             gpuMode = isa(inputs{1}, 'gpuArray');
