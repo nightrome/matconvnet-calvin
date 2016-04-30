6fcbcebada64fe213ac031190030534d31dfab08
Holger Caesar <holger.caesar@ed.ac.uk>
2016-04-30 14:12:08 +0100

    Removed unnecessary setup file

--- matconvnet-calvin/matlab/vl_compilenn_calvin.m
+++ matconvnet-calvin/matlab/vl_compilenn_calvin.m
@@ -2,16 +2,15 @@ function vl_compilenn_calvin()
 % vl_compilenn_calvin()
 %
 % Compile the C code provided in Matconvnet-calvin.
-% You also need to execute vl_compilenn('EnableGpu', true).
+% Matconvnet code needs to be compiled separately.
 %
 % Copyright by Holger Caesar, 2016
 
-root = vl_rootnn();
+root = fileparts(fileparts(mfilename('fullpath')));
 mexDir = fullfile(root, 'matlab', 'mex');
 mexOpts = {'-largeArrayDims', '-outdir', sprintf('"%s"', mexDir)};
 
 mex(mexOpts{:}, fullfile(root, 'matlab', 'labelpresence', 'labelPresence_backward.cpp'));
-mex(mexOpts{:}, fullfile(root, 'matlab', 'labelpresence', 'segmentationLabelPresence_backward.cpp'));
 mex(mexOpts{:}, fullfile(root, 'matlab', 'regiontopixel', 'regionToPixel_backward.cpp'));
 mex(mexOpts{:}, fullfile(root, 'matlab', 'roipool', 'roiPooling_forward.cpp'));
 mex(mexOpts{:}, fullfile(root, 'matlab', 'roipool', 'roiPooling_backward.cpp'));
