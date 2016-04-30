881cc8e25f326bdf26ba4a033f39833abd4aff0d
Holger Caesar <holger.caesar@ed.ac.uk>
2016-04-30 14:12:19 +0100

    Added general setup file

---
+++ vl_setupnn_all.m
@@ -0,0 +1,22 @@
+function vl_setupnn_all()
+% vl_setupnn_all()
+%
+% Add matconvnet, matconvnet-fcn and matconvnet-calvin to Matlab path.
+%
+% Copyright by Holger Caesar, 2016
+
+% Define paths
+root = fileparts(mfilename('fullpath'));
+matconvnetPath = fullfile(root, 'matconvnet', 'matlab');
+matconvnetFcnPath = fullfile(root, 'matconvnet-fcn');
+matconvnetCalvinPath = fullfile(root, 'matconvnet-calvin', 'matlab');
+
+% Add matconvnet
+addpath(matconvnetPath);
+vl_setupnn();
+
+% Add matconvnet-fcn
+addpath(genpath(matconvnetFcnPath));
+
+% Add matconvnet-calvin
+addpath(genpath(matconvnetCalvinPath));
