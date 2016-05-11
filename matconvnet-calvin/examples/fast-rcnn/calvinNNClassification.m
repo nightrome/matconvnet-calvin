0e85c837d08bdc68ba5e6ec4016deab91c405c2c
Holger Caesar <holger.caesar@ed.ac.uk>
2016-05-11 17:09:34 +0100

    simplified calvinNN examples

--- matconvnet-calvin/examples/fast-rcnn/calvinNNClassification.m
+++ matconvnet-calvin/examples/fast-rcnn/calvinNNClassification.m
@@ -1,157 +1,58 @@
-%% function calvinNNDetection
+% function calvinNNClassification
 
-% Example for classification on Pascal.
-
+% User inputs
 global MYDATADIR        % Directory of datasets
-global DATAopts         % Database specific paths
-global USEGPU           % Set in startup.m. Use GPU or not.
 
-%% Set training and test set
-trainName = 'trainval';
-testName = 'test';
+%%% Settings
+% Dataset
+vocYear = 2010;
+trainName = 'train';
+testName = 'val';
 
-
-%% Setup VOC data (should be done using original VOC code). Below is minimal working example
-DATAopts.year = 2007;
-DATAopts.dataset = 'VOC2007';
-DATAopts.datadir = [MYDATADIR 'VOCdevkit/' DATAopts.dataset '/'];
-DATAopts.resultsPath = [DATAopts.datadir 'Results/'];
-DATAopts.imgsetpath = [DATAopts.datadir 'ImageSets/Main/%s.txt'];
-DATAopts.clsimgsetpath=[DATAopts.datadir '/ImageSets/Main/%s_%s.txt'];
-DATAopts.imgpath = [DATAopts.datadir 'JPEGImages/%s.jpg'];
-DATAopts.resdir = [MYDATADIR 'VOCdevkit/results/' DATAopts.dataset '/'];
-DATAopts.annopath = [DATAopts.datadir 'VOCdevkit/Annotations/%s.xml'];
-DATAopts.annocachepath = [MYDATADIR 'VOCdevkit/local/VOC2007/%s_anno.mat'];
-DATAopts.classes={...
-    'aeroplane'
-    'bicycle'
-    'bird'
-    'boat'
-    'bottle'
-    'bus'
-    'car'
-    'cat'
-    'chair'
-    'cow'
-    'diningtable'
-    'dog'
-    'horse'
-    'motorbike'
-    'person'
-    'pottedplant'
-    'sheep'
-    'sofa'
-    'train'
-    'tvmonitor'};
-DATAopts.nclasses = length(DATAopts.classes);
-DATAopts.testset = testName;
-DATAopts.minoverlap=0.5;
-
-DATAopts.gStructPath = [DATAopts.resultsPath 'GStructs/'];
-
-
-%% Options for CNN training
+% Task
 nnOpts.fastRcnn = false;
 nnOpts.lossFnObjective = 'hinge';
 nnOpts.testFn = @testClassification;
+nnOpts.derOutputs = {'objective', 1};
+
+% General
 nnOpts.batchSize = 128;
 nnOpts.numSubBatches = 1;
 nnOpts.weightDecay = 5e-4;
 nnOpts.momentum = 0.9;
 nnOpts.numEpochs = 16;
-nnOpts.learningRate = cat(1, repmat(0.001, 12, 1), repmat(0.0001, 4, 1));
+nnOpts.learningRate = [repmat(1e-3, 12, 1); repmat(1e-4, 4, 1)];
+nnOpts.misc.netPath = fullfile(MYDATADIR, 'MatconvnetModels', 'imagenet-vgg-verydeep-16.mat');
+nnOpts.gpus = SelectIdleGpu();
 
-nnOpts.derOutputs = {'objective', 1};
-
-if USEGPU
-    nnOpts.gpus = SelectIdleGpu();
-else
-    nnOpts.gpus = [];
-end
-
-% output path
-nnOpts.expDir = [DATAopts.resultsPath ...
-    sprintf('FastRcnnMatconvnet/CalvinClassificationRun/')]
+% Setup data opts
+setupDataOpts(vocYear, testName);
+global DATAopts; % Database specific paths
+nnOpts.expDir = [DATAopts.resultsPath, sprintf('FastRcnnMatconvnet/CalvinClassificationRun/')];
 
 % Start from pretrained network
-net = load([MYDATADIR 'MatconvnetModels/imagenet-vgg-f.mat']);
-% net = load([MYDATADIR 'MatconvnetModels/imagenet-vgg-verydeep-16.mat']);
+net = load(nnOpts.misc.netPath);
 
-%% Setup the Imdb
-% Get images and labels
-set = trainName;
-[trainIms, ~] = textread(sprintf(DATAopts.imgsetpath,set),'%s %d');
-trainLabs = zeros(length(trainIms), DATAopts.nclasses);
+% Setup imdb
+imdb = setupImdbClassification(trainName, testName, net);
 
-% Get all testlabels
-if (~strcmp(set,'test') || DATAopts.year == 2007)
-    for classIdx = 1:DATAopts.nclasses
-        class = DATAopts.classes{classIdx};
-        [~, trainLabs(:,classIdx)] = textread(sprintf(DATAopts.clsimgsetpath,class,set),'%s %d');
-    end
+% Create calvinNN CNN class. Note nnOpts above for classification
+calvinn = CalvinNN(net, imdb, nnOpts);
 
-    trainLabs(trainLabs == 0) = -1;
-end
+% Create calvinNN CNN class. By default, network is transformed into fast-rcnn with bbox regression
+calvinn = CalvinNN(net, imdb, nnOpts);
 
-set = testName;
-[testIms, ~] = textread(sprintf(DATAopts.imgsetpath,set),'%s %d');
-testLabs = zeros(length(testIms), DATAopts.nclasses);
-
-% Get all testlabels
-if (~strcmp(set,'test') || DATAopts.year == 2007)
-    for classIdx = 1:DATAopts.nclasses
-        class = DATAopts.classes{classIdx};
-        [~, testLabs(:,classIdx)] = textread(sprintf(DATAopts.clsimgsetpath,class,set),'%s %d');
-    end
-
-    testLabs(testLabs == -1) = 0;
-end
-
-% Make train, val, and test set.
-numValIms = 500;
-allIms = cat(1, trainIms, testIms);
-allLabs = cat(1, trainLabs, testLabs);
-datasetIdx{1} = (1:length(trainIms)-numValIms)';  % Last numValIms are val set
-datasetIdx{2} = (length(trainIms)-numValIms+1:length(trainIms))';
-datasetIdx{3} = (length(trainIms)+1:length(allIms))';
-
-% Setup the classification imdb
-ImdbPascal = ImdbClassification(DATAopts.imgpath(1:end-6), ...        % path
-                                DATAopts.imgpath(end-3:end), ...      % image extension
-                                allIms, ...                           % all images
-                                allLabs, ...                          % all labels
-                                datasetIdx, ...                       % division into train/val/test
-                                net.normalization.averageImage, ...   % average image used to pretrain network
-                                20);                                  % num classes
-
-%% Create calvinNN CNN class. Note nnOpts above for classification
-calvinn = CalvinNN(net, ImdbPascal, nnOpts);
-
-%%%%%%%%%%%%%
-%% Train
-%%%%%%%%%%%%%
+% Train
 calvinn.train();
 
-%%%%%%%%%%%%%
-%% Test
-%%%%%%%%%%%%%
+% Test
 stats = calvinn.test();
 
+% Eval
+evalClassification();
 
-%% Do evaluation
-scores = zeros(size(testLabs));
-for i=1:length(stats.results)
-    scores(i,:) = stats.results(i).scores;
-end
-testLabsAp = testLabs;
-testLabsAp(testLabsAp == -1) = 0;
-[map, ap, conf] = MeanAveragePrecision(scores, testLabsAp);
 
-ap
-map
 
-%%
-save([nnOpts.expDir 'resultsEpochFinalTest.mat'], 'nnOpts', 'stats', 'ap', 'conf');
 
 if USEGPU
     exit
--- matconvnet-calvin/examples/fast-rcnn/calvinNNDetection.m
+++ matconvnet-calvin/examples/fast-rcnn/calvinNNDetection.m
@@ -1,209 +1,48 @@
 % function calvinNNDetection
 
+% User inputs
 global MYDATADIR        % Directory of datasets
-global DATAopts         % Database specific paths
-global USEGPU           % Set in startup.m. Use GPU or not.
 
-% Set training and test set
+%%% Settings
+% Dataset
+vocYear = 2010;
 trainName = 'train';
 testName = 'val';
 
-%%% Setup VOC data (should be done using original VOC code). Below is minimal working example
-DATAopts.year = 2010;
-DATAopts.dataset = sprintf('VOC%d', DATAopts.year);
-DATAopts.datadir = [MYDATADIR 'VOCdevkit/' DATAopts.dataset '/'];
-DATAopts.resultsPath = [DATAopts.datadir 'Results/'];
-DATAopts.imgsetpath = [DATAopts.datadir 'ImageSets/Main/%s.txt'];
-DATAopts.imgpath = [DATAopts.datadir 'JPEGImages/%s.jpg'];
-DATAopts.resdir = [MYDATADIR 'VOCdevkit/results/' DATAopts.dataset '/'];
-DATAopts.annopath = [DATAopts.datadir 'VOCdevkit/Annotations/%s.xml'];
-DATAopts.annocachepath = [MYDATADIR 'VOCdevkit/local/', DATAopts.dataset, '/%s_anno.mat'];
-DATAopts.classes={...
-    'aeroplane'
-    'bicycle'
-    'bird'
-    'boat'
-    'bottle'
-    'bus'
-    'car'
-    'cat'
-    'chair'
-    'cow'
-    'diningtable'
-    'dog'
-    'horse'
-    'motorbike'
-    'person'
-    'pottedplant'
-    'sheep'
-    'sofa'
-    'train'
-    'tvmonitor'};
-DATAopts.nclasses = length(DATAopts.classes);
-DATAopts.testset = testName;
-DATAopts.minoverlap = 0.5;
-DATAopts.gStructPath = [DATAopts.resultsPath, 'GStructs/'];
-
-
-%%% Options for CNN training
+% Task
 nnOpts.testFn = @testDetection;
+nnOpts.misc.overlapNms = 0.3;
+nnOpts.derOutputs = {'objective', 1, 'regressObjective', 1};
+
+% General
 nnOpts.batchSize = 2;
 nnOpts.numSubBatches = nnOpts.batchSize; % In Fast-RCNN numSubBatches needs to be equal to batchSize
 nnOpts.weightDecay = 5e-4;
 nnOpts.momentum = 0.9;
 nnOpts.numEpochs = 16;
-nnOpts.learningRate = cat(1, repmat(1e-3, 12, 1), repmat(1e-4, 4, 1));
-nnOpts.derOutputs = {'objective', 1, 'regressObjective', 1};
-overlapNms = 0.3;
+nnOpts.learningRate = [repmat(1e-3, 12, 1); repmat(1e-4, 4, 1)];
+nnOpts.misc.netPath = fullfile(MYDATADIR, 'MatconvnetModels', 'imagenet-vgg-verydeep-16.mat');
+nnOpts.gpus = SelectIdleGpu();
 
-if USEGPU
-    nnOpts.gpus = 1; %SelectIdleGpu();
-else
-    nnOpts.gpus = [];
-end
-
-% output path
+% Setup data opts
+setupDataOpts(vocYear, testName);
+global DATAopts; % Database specific paths
 nnOpts.expDir = [DATAopts.resultsPath, sprintf('FastRcnnMatconvnet/CalvinDetectionRun/')];
 
 % Start from pretrained network
-net = load([MYDATADIR 'MatconvnetModels/imagenet-vgg-verydeep-16.mat']);
+net = load(nnOpts.misc.netPath);
 
-%%% Setup the Imdb
-% Get and test images
-trainIms = textread(sprintf(DATAopts.imgsetpath, trainName), '%s');
-testIms = textread(sprintf(DATAopts.imgsetpath, testName), '%s');
+% Setup imdb
+imdb = setupImdbDetection(trainName, testName, net);
 
-% Make train, val, and test set. For Pascal, I illegally use part of the test images
-% as validation set. This is to match Girshick performance while still having
-% meaningful graphs for the validation set.
-% Note: allIms are just all images. datasetIdx determines how these are divided over
-% train, val, and test.
-allIms = cat(1, trainIms, testIms);
-datasetIdx{1} = (1:length(trainIms))';  % Jasper: Use all training images. Only for comparison Pascal Girshick
-datasetIdx{2} = (length(trainIms)+1:length(trainIms)+501)'; % Use part of the test images for validation. Not entirely legal, but otherwise it will take much longer to get where we want.
-datasetIdx{3} = (length(trainIms)+1:length(allIms))';
+% Create calvinNN CNN class. By default, network is transformed into fast-rcnn with bbox regression
+calvinn = CalvinNN(net, imdb, nnOpts);
 
-ImdbPascal = ImdbDetectionFullSupervision(DATAopts.imgpath(1:end-6), ...        % path
-    DATAopts.imgpath(end-3:end), ...      % image extension
-    DATAopts.gStructPath, ...             % gStruct path
-    allIms, ...                           % all images
-    datasetIdx, ...                       % division into train/val/test
-    net.meta.normalization.averageImage);      % average image used to pretrain network
-
-% Usually instance weighting gives better performance. But not Girshick style
-% ImdbPascal.SetInstanceWeighting(true);
-
-%%% Create calvinNN CNN class. By default, network is transformed into fast-rcnn with bbox regression
-calvinn = CalvinNN(net, ImdbPascal, nnOpts);
-
-%%%%%%%%%%%%%
-%%% Train
-%%%%%%%%%%%%%
+% Train
 calvinn.train();
 
-%%%%%%%%%%%%%
-%%% Test
-%%%%%%%%%%%%%
+% Test
 stats = calvinn.test();
 
-%%%%%%%%%%%%%
-%%% Eval
-%%%%%%%%%%%%%
-clear recall prec ap upperBound
-
-% get image sizes
-for i=length(testIms):-1:1
-    im = imread(sprintf(DATAopts.imgpath, testIms{i}));
-    imSizes(i,:) = size(im);
-end
-
-for cI = 1:20
-    %
-    currBoxes = cell(length(testIms), 1);
-    currScores = cell(length(testIms), 1);
-    for i=1:length(testIms)
-        currBoxes{i} = stats.results(i).boxes{cI+1};
-        currScores{i} = stats.results(i).scores{cI+1};
-    end
-    
-    [currBoxes, fileIdx] = Cell2Matrix(gather(currBoxes));
-    [currScores, fileIdx2] = Cell2Matrix(gather(currScores));
-    
-    assert(isequal(fileIdx, fileIdx2)); % Should be equal
-    
-    currFilenames = testIms(fileIdx);
-    
-    [~, sI] = sort(currScores, 'descend');
-    currScores = currScores(sI);
-    currBoxes = currBoxes(sI,:);
-    currFilenames = currFilenames(sI);
-    
-    %     ShowImageRects(currBoxes(1:32, [2 1 4 3]), 4, 4, currFilenames(1:32), currScores(1:32));
-    
-    %
-    [recall{cI}, prec{cI}, ap(cI,1), upperBound{cI}] = ...
-        DetectionToPascalVOCFiles(testName, cI, currBoxes, currFilenames, currScores, ...
-        'FastRcnnMatconvnet', 1, overlapNms);
-    ap(cI)
-end
-
-ap
-mean(ap)
-
-% get image sizes to refit regressed boxes
-for i=length(testIms):-1:1
-    im = ImageRead(testIms{i});
-    imSizes(i,:) = size(im);
-end
-
-if isfield(stats.results(1), 'boxesRegressed')
-    
-    for cI = 1:20
-        %
-        currBoxes = cell(length(testIms), 1);
-        currScores = cell(length(testIms), 1);
-        for i=1:length(testIms)
-            % Get regressed boxes and refit them to the image
-            currBoxes{i} = stats.results(i).boxesRegressed{cI+1};
-            currBoxes{i}(:,1) = max(currBoxes{i}(:,1), 1);
-            currBoxes{i}(:,2) = max(currBoxes{i}(:,2), 1);
-            currBoxes{i}(:,3) = min(currBoxes{i}(:,3), imSizes(i,2));
-            currBoxes{i}(:,4) = min(currBoxes{i}(:,4), imSizes(i,1));
-            
-            currScores{i} = stats.results(i).scoresRegressed{cI+1};
-        end
-        
-        [currBoxes, fileIdx] = Cell2Matrix(gather(currBoxes));
-        [currScores, fileIdx2] = Cell2Matrix(gather(currScores));
-        
-        %     isequal(fileIdx, fileIdx2) % Should be equal
-        
-        currFilenames = testIms(fileIdx);
-        
-        [~, sI] = sort(currScores, 'descend');
-        currScores = currScores(sI);
-        currBoxes = currBoxes(sI,:);
-        currFilenames = currFilenames(sI);
-        
-        %     ShowImageRects(currBoxes(1:32, [2 1 4 3]), 4, 4, currFilenames(1:32), currScores(1:32));
-        
-        %
-        [recall{cI}, prec{cI}, apRegressed(cI,1), upperBound{cI}] = ...
-            DetectionToPascalVOCFiles(testName, cI, currBoxes, currFilenames, currScores, ...
-            'FastRcnnMatconvnet', 1, overlapNms);
-        apRegressed(cI)
-    end
-    
-    apRegressed
-    mean(apRegressed)
-else
-    apRegressed = 0;
-end
-
-%
-save([nnOpts.expDir 'resultsEpochFinalTest.mat'], 'nnOpts', 'stats', 'ap', 'apRegressed');
-
-if USEGPU
-    % Exit to free GPU memory
-    exit
-end
+% Eval
+evalDetection(testName, testIms, testLabs, stats, nnOpts);
