44ef5fd5956cefc1aef2c611cfe8b46fdaeb7c71
Holger Caesar <holger.caesar@ed.ac.uk>
2016-07-27 18:41:40 +0100

    Added missing files and their compiling directives

--- matconvnet-calvin/matlab/vl_compilenn_calvin.m
+++ matconvnet-calvin/matlab/vl_compilenn_calvin.m
@@ -10,7 +10,11 @@ root = fileparts(fileparts(mfilename('fullpath')));
 mexDir = fullfile(root, 'matlab', 'mex');
 mexOpts = {'-largeArrayDims', '-outdir', sprintf('"%s"', mexDir)};
 
+% E2S2-related
 mex(mexOpts{:}, fullfile(root, 'matlab', 'labelpresence', 'labelPresence_backward.cpp'));
 mex(mexOpts{:}, fullfile(root, 'matlab', 'regiontopixel', 'regionToPixel_backward.cpp'));
 mex(mexOpts{:}, fullfile(root, 'matlab', 'roipool', 'roiPooling_forward.cpp'));
 mex(mexOpts{:}, fullfile(root, 'matlab', 'roipool', 'roiPooling_backward.cpp'));
+
+% Misc
+mex(mexOpts{:}, fullfile(root, 'matlab', 'misc', 'computeBlobOverlapAnyPair.cpp'));
