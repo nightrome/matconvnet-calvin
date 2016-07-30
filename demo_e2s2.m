0cbdc22ab26dbb2a1bab4d258187348503ce88c7
Holger Caesar <nightrome@users.noreply.github.com>
2016-07-30 08:17:06 +0200

    Fixed bug #1

--- demo_e2s2.m
+++ demo_e2s2.m
@@ -9,8 +9,7 @@ rootFolder = calvin_root();
 glBaseFolder = fullfile(rootFolder, 'data');
 glDatasetFolder = fullfile(glBaseFolder, 'Datasets');
 glFeaturesFolder = fullfile(glBaseFolder, 'Features');
-dataset = SiftFlowDataset(); % Requires global folders to be set first
-labelingsFolder = fullfile(glFeaturesFolder, 'CNN-Models', 'E2S2', dataset.name, 'Run1', sprintf('%s_e2s2_run1_exp1', dataset.name), 'labelings-test-epoch-30');
+labelingsFolder = fullfile(glFeaturesFolder, 'CNN-Models', 'E2S2', 'SiftFlow', 'Run1', sprintf('%s_e2s2_run1_exp1', 'SiftFlow'), 'labelings-test-epoch-30');
 
 % Download dataset
 downloadSiftFlow();
@@ -22,10 +21,10 @@ downloadNetwork();
 downloadSelectiveSearch();
 
 % Extract region proposals and labels
-setupE2S2Regions('dataset', dataset);
+setupE2S2Regions();
 
 % Train and test network
-e2s2_wrapper_SiftFlow('dataset', dataset);
+e2s2_wrapper_SiftFlow();
 
 % Show example segmentation
 fileList = dirSubfolders(labelingsFolder);
