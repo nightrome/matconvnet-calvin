fd02e865e0174a26f608088c9628c58828f1aae5
Holger Caesar <holger.caesar@ed.ac.uk>
2016-08-08 17:20:43 +0100

    Fixed folders in compile script

--- matconvnet-calvin/matlab/vl_compilenn_calvin.m
+++ matconvnet-calvin/matlab/vl_compilenn_calvin.m
@@ -7,15 +7,16 @@ function vl_compilenn_calvin()
 % Copyright by Holger Caesar, 2016
 
 root = calvin_root();
-mexDir = fullfile(root, 'matlab', 'mex');
+codeDir = fullfile(root, 'matconvnet-calvin', 'matlab');
+mexDir = fullfile(codeDir, 'mex');
 mexOpts = {'-largeArrayDims', '-outdir', sprintf('"%s"', mexDir)};
 
 % E2S2-related
-mex(mexOpts{:}, fullfile(root, 'matlab', 'labelpresence', 'labelPresence_backward.cpp'));
-mex(mexOpts{:}, fullfile(root, 'matlab', 'regiontopixel', 'regionToPixel_backward.cpp'));
-mex(mexOpts{:}, fullfile(root, 'matlab', 'roipool', 'roiPooling_forward.cpp'));
-mex(mexOpts{:}, fullfile(root, 'matlab', 'roipool', 'roiPooling_backward.cpp'));
+mex(mexOpts{:}, fullfile(codeDir, 'labelpresence', 'labelPresence_backward.cpp'));
+mex(mexOpts{:}, fullfile(codeDir, 'regiontopixel', 'regionToPixel_backward.cpp'));
+mex(mexOpts{:}, fullfile(codeDir, 'roipool', 'roiPooling_forward.cpp'));
+mex(mexOpts{:}, fullfile(codeDir, 'roipool', 'roiPooling_backward.cpp'));
 
 % Misc
-mex(mexOpts{:}, fullfile(root, 'matlab', 'misc', 'computeBlobOverlapAnyPair.cpp'));
-mex(mexOpts{:}, fullfile(root, 'matlab', 'misc', 'scoreBlobIoUs.cpp'));
+mex(mexOpts{:}, fullfile(codeDir, 'misc', 'computeBlobOverlapAnyPair.cpp'));
+mex(mexOpts{:}, fullfile(codeDir, 'misc', 'scoreBlobIoUs.cpp'));
