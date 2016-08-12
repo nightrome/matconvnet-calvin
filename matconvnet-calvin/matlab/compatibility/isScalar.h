6a8c181e118e3eaf3289e001df2dd6ed634d3019
Holger Caesar <holger.caesar@ed.ac.uk>
2016-08-12 18:27:23 +0100

    Workaround for the case where matrices have more than 2 dimensions

--- matconvnet-calvin/matlab/compatibility/isScalar.h
+++ matconvnet-calvin/matlab/compatibility/isScalar.h
@@ -1,5 +1,6 @@
 /*
- * Workaround in case Matlab's mxIsScalar is not available
+ * Workaround in case Matlab's mxIsScalar is not available.
+ * Only works for <= 2 dimensional matrices, otherwise returns true.
  *
  * Copyright by Holger Caesar, 2016
  */
@@ -9,8 +10,9 @@ bool isScalar(const mxArray *arr) {
     int ncols = mxGetN(arr);
     
     // Check that this matrix has <= 2 dimensions
-    if (mxGetNumberOfDimensions(arr) <= 2) {
-        mexErrMsgTxt("Error: Compatibility script isScalar() does not support matrices with >2 dimensions!");
+    if (mxGetNumberOfDimensions(arr) > 2) {
+        return false;
+//         mexErrMsgTxt("Error: Compatibility script isScalar() does not support matrices with >2 dimensions!");
     }
     
     // Check format
