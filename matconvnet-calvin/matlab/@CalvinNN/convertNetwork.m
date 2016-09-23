4a6057777d038dc67b0e61df4073dc667297868c
Holger Caesar <holger.caesar@ed.ac.uk>
2016-09-23 10:59:58 +0100

    Removed batch normalization option

--- matconvnet-calvin/matlab/@CalvinNN/convertNetwork.m
+++ matconvnet-calvin/matlab/@CalvinNN/convertNetwork.m
@@ -8,39 +8,12 @@ function convertNetwork(obj)
 
 fprintf('Converting test network to train network (dropout, loss, etc.)...\n');
 
-% Add dropout or batch normalization or a mix
-if isfield(obj.nnOpts.misc, 'batchNorm') && obj.nnOpts.misc.batchNorm
-    % Add batch normalization after all fc and conv layers
-    
-    fprintf('Adding batch normalization to network...\n');
-    
-    % Get ReLUs
-    reluInds = find(arrayfun(@(x) isa(x.block, 'dagnn.ReLU'), obj.net.layers));
-    
-    for i = 1 : numel(reluInds)
-        % Relu
-        reluIdx = reluInds(i);
-        addBatchNorm(obj, reluIdx);
-    end
-elseif isfield(obj.nnOpts.misc, 'batchNormFC7') && obj.nnOpts.misc.batchNormFC7
-    % Insert DropOut only for fc6
-    fprintf('Adding dropout to network...\n');
-    dropout6Layer = dagnn.DropOut();
-    insertLayer(obj.net, 'relu6', 'fc7', 'dropout6', dropout6Layer);
-    
-    % Insert batch normalization for fc7
-    fprintf('Adding batch normalization to network...\n');
-    reluIdx = obj.net.getLayerIndex('relu7');
-    addBatchNorm(obj, reluIdx);
-else
-    % Add dropout layers after relu6 and relu7
-    fprintf('Adding dropout to network...\n');
-    
-    dropout6Layer = dagnn.DropOut();
-    dropout7Layer = dagnn.DropOut();
-    insertLayer(obj.net, 'relu6', 'fc7', 'dropout6', dropout6Layer);
-    insertLayer(obj.net, 'relu7', 'fc8', 'dropout7', dropout7Layer);
-end
+% Add dropout layers after relu6 and relu7
+fprintf('Adding dropout to network...\n');
+dropout6Layer = dagnn.DropOut();
+dropout7Layer = dagnn.DropOut();
+insertLayer(obj.net, 'relu6', 'fc7', 'dropout6', dropout6Layer);
+insertLayer(obj.net, 'relu7', 'fc8', 'dropout7', dropout7Layer);
 
 % Rename variable prob to x%d+ style to make insertLayer work
 % (Required for beta18 or later matconvnet default networks)
@@ -63,7 +36,7 @@ switch obj.nnOpts.lossFnObjective
         objectiveName = obj.net.layers(obj.net.getLayerIndex('hingeloss')).outputs;
         obj.net.renameVar(objectiveName, 'objective');
     otherwise
-        error('Wrong loss specified');
+        error('Unknown loss specified');
 end
 
 % Adapt number of classes in softmaxloss layer from 1000 to numClasses
@@ -85,51 +58,4 @@ end
 % Modify for Fast Rcnn (ROI pooling, bbox regression etc.)
 if obj.nnOpts.fastRcnn
     obj.convertNetworkToFastRcnn();
-end
-
-function addBatchNorm(obj, reluIdx)
-% function addBatchNorm(obj, reluIdx)
-%
-% Adds a batch normalization layer before the specified relu
-
-reluLayerName = obj.net.layers(reluIdx).name;
-reluInputIdx = obj.net.layers(reluIdx).inputIndexes;
-assert(numel(reluInputIdx) == 1);
-
-% Left layer
-leftLayerIdx = find(arrayfun(@(x) ismember(reluInputIdx, x.outputIndexes), obj.net.layers));
-assert(numel(leftLayerIdx) == 1);
-leftLayerName = obj.net.layers(leftLayerIdx).name;
-leftParamIdx = obj.net.layers(leftLayerIdx).paramIndexes(1);
-numChannels = size(obj.net.params(leftParamIdx).value, 4);
-if isfield(obj.nnOpts.misc, 'roiPool') && ... %TODO: make more general or outsource to E2S2NN
-        ~obj.nnOpts.misc.roiPool.freeform.shareWeights && strcmp(reluLayerName, 'relu7')
-    numChannels = numChannels * 2;
-end
-
-% Insert new layer
-layerBlock = dagnn.BatchNorm('numChannels', numChannels);
-layerParamValues = layerBlock.initParams();
-layerName = sprintf('bn_%s', reluLayerName);
-layerParamNames = cell(1, numel(layerParamValues));
-for i = 1 : numel(layerParamValues)
-    layerParamNames{i} = sprintf('%s_%d', layerName, i);
-end
-insertLayer(obj.net, leftLayerName, reluLayerName, layerName, layerBlock, {}, {}, layerParamNames);
-
-for i = 1 : numel(layerParamValues)
-    paramIdx = obj.net.getParamIndex(layerParamNames{i});
-    obj.net.params(paramIdx).value = layerParamValues{i};
-    
-    % Learning rate and weight decay as taken from Matconvnet's cnn_imagenet_init.m
-    if i == 1
-        obj.net.params(paramIdx).learningRate = 2;
-    elseif i == 2
-        obj.net.params(paramIdx).learningRate = 1;
-    elseif i == 3
-        obj.net.params(paramIdx).learningRate = 0.05;
-    else
-        error('Error: Invalid number of batch norm parameters!');
-    end
-    obj.net.params(paramIdx).weightDecay = 0;
 end
