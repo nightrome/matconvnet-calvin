4fd6f722975ffbf50c495f7fc239b3a3e66a97d7
Holger Caesar <holger.caesar@ed.ac.uk>
2016-10-01 16:08:38 +0100

    Fixed export script to include (corrected) folders

--- matconvnet-calvin/matlab/export/export_model.m
+++ matconvnet-calvin/matlab/export/export_model.m
@@ -13,15 +13,15 @@ if strcmp(modelName, 'frcn')
     exportFilesPrefix = fullfile(glBaseFolder, 'Features', 'CNN-Models', 'FRCN', 'VOC2010', 'VOC2010-testRelease');
 elseif strcmp(modelName, 'fcn')
     exportName = 'FCN_SiftFlow_model';
-    exportFiles = {fullfile('labelings-test-epoch50', '*'), 'net-epoch-50.mat', 'net-opts.mat', 'net-train.pdf', 'log.txt', 'stats-test-epoch50.mat'};
+    exportFiles = {'labelings-test-epoch50', 'net-epoch-50.mat', 'net-opts.mat', 'net-train.pdf', 'log.txt', 'stats-test-epoch50.mat'};
     exportFilesPrefix = fullfile(glBaseFolder, 'Features', 'CNN-Models', 'FCN', 'SiftFlow', 'fcn16s-testRelease');
 elseif strcmp(modelName, 'e2s2_fast')
     exportName = 'E2S2_SiftFlow_model_fast';
-    exportFiles = {fullfile('labelings-test-epoch10', '*'), 'net-epoch-10.mat', 'net-opts.mat', 'net-train.pdf', 'log.txt', 'stats-test-epoch10.mat'};
+    exportFiles = {'labelings-test-epoch10', 'net-epoch-10.mat', 'net-opts.mat', 'net-train.pdf', 'log.txt', 'stats-test-epoch10.mat'};
     exportFilesPrefix = fullfile(glBaseFolder, 'Features', 'CNN-Models', 'E2S2', 'SiftFlow', 'Run1', 'SiftFlow_e2s2_run1_exp1');
 elseif strcmp(modelName, 'e2s2_full')
     exportName = 'E2S2_SiftFlow_model_full';
-    exportFiles = {fullfile('labelings-test-epoch25', '*'), 'net-epoch-25.mat', 'net-opts.mat', 'net-train.pdf', 'log.txt', 'stats-test-epoch25.mat'};
+    exportFiles = {'labelings-test-epoch25', 'net-epoch-25.mat', 'net-opts.mat', 'net-train.pdf', 'log.txt', 'stats-test-epoch25.mat'};
     exportFilesPrefix = fullfile(glBaseFolder, 'Features', 'CNN-Models', 'E2S2', 'SiftFlow', 'Run1', 'SiftFlow_e2s2_run1_exp2');
 else
     error('Error: Unknown model: %s', modelName);
@@ -36,8 +36,8 @@ end
 % Create zip file
 exportFilePath = fullfile(exportFolder, [exportName, '.zip']);
 if exist(exportFilePath, 'file')
-    fprintf('Using existing zip file: %s', exportFilePath);
+    fprintf('Using existing zip file: %s\n', exportFilePath);
 else
-    fprintf('Creating zip file: %s', exportFilePath);
+    fprintf('Creating zip file: %s\n', exportFilePath);
     zip(exportFilePath, strcat(exportFilesPrefix, filesep, exportFiles));
 end
